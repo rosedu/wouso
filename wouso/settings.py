e16e48dfc29ce34a2266b687594d3e8dbbb85ce1
Mircea Ştefan Ghideu <mirhciulica@archlinux.us>
2011-01-20 21:29:43 +0200

    new database setup syntax

--- wouso/settings.py
+++ wouso/settings.py
@@ -10,12 +10,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'wouso'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'alex'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'NAME': 'wouso',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'root',
+        'PASSWORD': 'alex',
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -101,6 +103,7 @@ INSTALLED_APPS = (
     'wouso.games.challenge',
     'wouso.games.wquest',
     'wouso.extra.activity',
+    #'wouso.core.profile.templatetags.profile_tags',
     'facebookconnect'
 )
 
