df037e0d7912e18cb00bbec48e735e5a2a9a2ac2
Alex Eftimie <alex@rosedu.org>
2011-02-26 17:03:36 +0200

    Initial new design cleanup

--- wouso/core/profile/forms.py
+++
@@ -1,8 +0,0 @@
-from core.profile.models import Message
-from django.forms import ModelForm
-
-class MessageForm(ModelForm):
-    """ Message form for composing / replying """
-    class Meta:
-        model = Message
-        fields = ('subject', 'text')
