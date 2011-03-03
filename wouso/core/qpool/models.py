cb8247e2dfa6879a2e2b17b204bf8326f382e8ca
Vlad Dumitrescu <vlad@dumitrescu.ro>
2011-03-03 21:38:59 +0200

    qpool question model

--- wouso/core/qpool/models.py
+++ wouso/core/qpool/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField()
+    # tags could have different types ("date" for questions tagged with a date for qotd)
+    type = models.CharField(default="default")
+    
+class Question(models.Model):
+    text = models.TextField()
+    proposedby = models.ForeignKey(User, related_name="%(app_label)s_%(class)s_related")
+    endorsedby = models.ForeignKey(User, null=True, related_name="%(app_label)s_%(class)s_related")
+    active = models.BooleanField()
+    tags = models.ManyToManyField(Tag)
+    # a dynamic question would have its code run before returning it to the caller
+    type = models.CharField(max_length=1, choices=(("S", "static"), ("D", "dynamic")))
+    code = models.TextField(blank=True)
+    
+    class Meta:
+        abstract = True
+
+class RadioQuestion(Question):
+    pass
+
+class CheckQuestion(Question):
+    pass
