23bdbaedf8a850f4fb3f28de310c49fc3d6ba3fa
Alex Eftimie <alex@eftimie.ro>
2011-03-04 13:54:50 +0200

    Qotd almost there - missing qpool and scoring

---
+++ wouso/games/qotd/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+""" Form for data validation """
+class QotdForm(forms.Form):
+    def __init__(self, qotd, data = None): 
+        super(QotdForm, self).__init__(data)
+        data = [[a.id, a.text] for a in qotd.answers]
+        self.fields['answers'].choices = data
+    
+    answers = forms.ChoiceField(widget=forms.RadioSelect(), 
+        choices=[])
