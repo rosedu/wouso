9668f5b0b24f3665985855ddb5d8512f098ddf7c
Mircea Ştefan Ghideu <shtephan10@gmail.com>
2011-03-05 13:57:59 +0200

    Initial work for Activity module

--- wouso/interface/activity/models.py
+++ wouso/interface/activity/models.py
@@ -1,3 +1,15 @@
+from datetime import datetime
 from django.db import models
+from wouso.core.game.models import Game
+from wouso.core.user.models import User
+from wouso.interface import logger
 
-# Create your models here.
+class Activity(models.Model):
+    user_from = models.ForeignKey(User)
+    user_to = models.ForeignKey(User)
+    message = models.CharField(max_length=140)
+    game = models.ForeignKey(Game)
+
+    def get_activities(start, end, filter=none, self):
+        # TODO: implement Activities filtering
+        return Activities.objects.all()[-end:-start].reverse()
--- wouso/interface/activity/views.py
+++ wouso/interface/activity/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from wouso.interface import Activity
+from wouso.interface import logger
+
+def activities(request):
+    list = Activity.get_activities(0,10)
+    return render_to_response('activity.html',
+            {'activity': list},
+            context_instance=RequestContext(request))
