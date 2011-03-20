126d97a31de357f43d54a4c69fa50f8dcb241727
Vlad Dumitrescu <vlad@dumitrescu.ro>
2011-03-20 13:30:36 +0200

    core.qpool.get_questions_with_tags stub

---
+++ wouso/core/qpool/__init__.py
@@ -0,0 +1,4 @@
+def get_questions_with_tags(tlist):
+    # TODO: 
+    #print models.Question.objects.filter(tags__contains='challenge')
+    #print [x.question_set for x in models.Tag.objects.filter(name='challenge')]
