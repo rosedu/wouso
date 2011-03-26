cbdfb34e7f29fcc5fac3459ba647ed9468935022
Mircea Ştefan Ghideu <mirhciulica@archlinux.us>
2011-03-26 18:54:43 +0200

    restore dump

--- wouso/db_dump.py
+++ wouso/db_dump.py
@@ -6,7 +6,6 @@ from django.core import serializers
 from django.contrib.auth.models import User
 from core.qpool.models import Question, Answer
 
-json_serializer = serializers.get_serializer("json")()
 
 def dump_db():
     dump_file = open('db_dump.txt', 'w')
@@ -14,11 +13,16 @@ def dump_db():
     qpool = Question.objects.all()
     qpool_answ = Answer.objects.all()
     all_objects = list(list(users) + list(qpool) + list(qpool_answ))
+    json_serializer = serializers.get_serializer("json")()
     json_serializer.serialize(all_objects, ensure_ascii=True, stream=dump_file)
     dump_file.close()
 
 def restore_db():
-    pass
+    dump_file = open('db_dump.txt', 'r')
+    data = dump_file.read()
+    all_objects = serializers.deserialize("json", data)
+    for object in all_objects:
+        object.save()
 
 def usage():
     print('Usage: %s command\n' % argv[0])
