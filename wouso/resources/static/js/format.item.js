97bc64cab8f64048dd65bf95d023128e8ed4aff8
Dan Serban <dserban01@gmail.com>
2011-03-27 02:20:22 +0200

    [interface] autocomplete artifact image fix

--- wouso/resources/static/js/format.item.js
+++ wouso/resources/static/js/format.item.js
@@ -4,8 +4,8 @@ function formatItem( row ) {
     str = str.replace(/[^\/]/g,"");
     str = str.replace(/\//g,"../");
     
-    return row[0] + "<br /><img align=\"absmiddle\" src=\""+ 
-        str+row[4]+"\" /> "+
+    return row[0] + "<br />"+ 
+        row[4]+" "+
         "<small><i>" + row[2] + " (" + row[3] + ") " +
         row[1] + " puncte</small></i>";
 }
