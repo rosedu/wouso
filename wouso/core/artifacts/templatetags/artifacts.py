a2115bc1101f3e667220bfd0d2ec8e1d6f00d098
Alex Eftimie <alex@eftimie.ro>
2011-07-27 13:52:20 +0300

    make vanilla install kinda work

--- wouso/core/artifacts/templatetags/artifacts.py
+++ wouso/core/artifacts/templatetags/artifacts.py
@@ -4,7 +4,8 @@ register = template.Library()
 
 @register.simple_tag
 def artifact(artifact):
-    if artifact.path[0] == '/':
-        return '<img class="artifact" src="%s" />' % artifact.path
-    
-    return '<div class="artifact artifact-%s"></div>' % artifact.path
+    path = artifact.path if artifact else ' '
+    if path[0] == '/':
+        return '<img class="artifact" src="%s" />' % path
+
+    return '<div class="artifact artifact-%s"></div>' % path
