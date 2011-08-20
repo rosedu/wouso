b78b57df91e0338f204d1fbb95a301e836cd5464
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-08-20 19:50:19 +0300

    interface for uploading questions from a file

--- wouso/interface/cpanel/urls.py
+++ wouso/interface/cpanel/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'wouso.interface.cpanel.views.dashboard'),
+    url(r'^importer/$', 'wouso.interface.cpanel.views.importer', name='importer'),
+    url(r'^importer/send$', 'wouso.interface.cpanel.views.import_from_upload', name='importer'),
 )
-
