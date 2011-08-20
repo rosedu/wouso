80ddf409314edc76b16a51b12d3873014e1a4ca9
Alex Morega <alex@grep.ro>
2011-08-20 15:05:46 +0300

    configure sphinx so that it can import our code

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,9 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+sys.path.insert(0, os.path.join(os.path.abspath('.'), '..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wouso.settings'
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
