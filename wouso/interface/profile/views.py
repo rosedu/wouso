0b99e16205ce2f10d2d1a8943a0fc6cbda6361d6
Adrian Stratulat <adrian.stratulat@cti.pub.ro>
2011-08-20 15:47:46 +0300

    [interface] added latest activities to profile page

--- wouso/interface/profile/views.py
+++ wouso/interface/profile/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from django.db.models import Q
 from wouso.interface import render_response
 from wouso.core.user.models import UserProfile
 from wouso.interface.activity.models import Activity
@@ -14,9 +15,10 @@ def profile(request):
 def user_profile(request, id):
     try:
         profile = UserProfile.objects.get(id=id)
+        activity = Activity.objects.filter(Q(user_to=id) | Q(user_from=id)).order_by('-timestamp')[:10]
     except UserProfile.DoesNotExist:
         raise Http404
 
     return render_response('profile/profile.html',
         request,
-        {'profile': profile})
+        {'profile': profile, 'activity': activity})
