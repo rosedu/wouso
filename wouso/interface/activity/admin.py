6fd56b0f1712dfa5831292ec4caa890ddf8089e3
Mircea Ştefan Ghideu <mirhciulica@archlinux.us>
2011-08-20 19:08:52 +0300

    improved activity module admin page

--- wouso/interface/activity/admin.py
+++ wouso/interface/activity/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from models import Activity
 
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('timestamp', 'user_from', 'user_to', 'message', 'game')
+    list_display = ('timestamp', 'user_from', 'user_to', 'fromgame', 'message')
 
 admin.site.register(Activity, ActivityAdmin)
