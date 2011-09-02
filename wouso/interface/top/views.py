f7d6172dd92ba3cf2b3205f4b262a0dcb4eceef0
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-09-02 20:42:08 +0300

    [interface/top/views] get rid of semicolons

--- wouso/interface/top/views.py
+++ wouso/interface/top/views.py
@@ -5,47 +5,47 @@ from django.template import RequestContext
 from django.http import Http404
 from wouso.interface.top.models import History, TopUser
 
-PERPAGE = 5;
+PERPAGE = 5
 def gettop(request, toptype, sortcrit, page):
     # toptype = 0 means overall top
     # toptype = 1 means top for 1 week
     # sortcrit = 0 means sort by points descending
     # sortcrit = 1 means sort by progress descending
     # sortcrit = 2 means sort by last_seen descending
-    next_page = 0;
-    prev_page = 0;
+    next_page = 0
+    prev_page = 0
     try:
-        toptypeno = int(toptype);
+        toptypeno = int(toptype)
     except:
-        toptypeno = 0;
+        toptypeno = 0
     try:
-        sortcritno = int(sortcrit);
+        sortcritno = int(sortcrit)
     except:
-        sortcritno = 0;
+        sortcritno = 0
     try:
-        pageno = int(page);
+        pageno = int(page)
     except:
-        pageno = 1;
+        pageno = 1
     if pageno < 0:
-        raise Http404;
+        raise Http404
 
-    allUsers = TopUser.objects.order_by('-points')[(pageno-1)*PERPAGE:pageno*PERPAGE];
+    allUsers = TopUser.objects.order_by('-points')[(pageno-1)*PERPAGE:pageno*PERPAGE]
     if (allUsers.count() == 0):
-        raise Http404;
+        raise Http404
     if sortcritno == 1:
-        allUsers = sorted(TopUser.objects.all(), key = lambda p: p.progress, reverse=True)[(pageno-1)*PERPAGE:pageno*PERPAGE];
+        allUsers = sorted(TopUser.objects.all(), key = lambda p: p.progress, reverse=True)[(pageno-1)*PERPAGE:pageno*PERPAGE]
     if sortcritno == 2:
-        allUsers = TopUser.objects.order_by('-last_seen')[(pageno-1)*PERPAGE:pageno*PERPAGE];
+        allUsers = TopUser.objects.order_by('-last_seen')[(pageno-1)*PERPAGE:pageno*PERPAGE]
 
-    count = UserProfile.objects.count();
+    count = UserProfile.objects.count()
     if (count > pageno*PERPAGE+PERPAGE):
-        next_page = pageno + 1;
+        next_page = pageno + 1
     if pageno > 1:
-        prev_page = pageno - 1;
+        prev_page = pageno - 1
     return render_to_response('top/maintop.html',
                            {'allUsers':      allUsers,
                             'toptype':       toptypeno,
                             'sortcrit':      sortcritno,
                             'page_top_next': next_page,
                             'page_top_prev': prev_page},
-                           context_instance=RequestContext(request));
+                           context_instance=RequestContext(request))
