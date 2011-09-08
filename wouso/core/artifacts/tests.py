184f5b8c0bec02f422971046741a4585db4318e5
Alex Eftimie <alex@eftimie.ro>
2011-09-08 15:25:22 +0300

    [core] improve artifact models

---
+++ wouso/core/artifacts/tests.py
@@ -0,0 +1,14 @@
+import unittest
+from django.db import IntegrityError
+from models import *
+
+class ArtifactTestCase(unittest.TestCase):
+
+    def testArtifactCreateUnique(self):
+        """ Test if we cannot create two artifacts with the same name in a group
+        """
+        group = Group.objects.create(name='gigi')
+
+        a1 = Artifact.objects.create(group=group, name='name')
+
+        self.assertRaises(IntegrityError, Artifact.objects.create, group=group, name='name')
