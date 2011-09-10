6724ef3a6ba63e519b1598bdf31d2c00bb9711fc
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-09-10 14:31:56 +0300

    [challenge] expired fix duplicate (mexican) signal

--- wouso/games/challenge/models.py
+++ wouso/games/challenge/models.py
@@ -218,20 +218,11 @@ class Challenge(models.Model):
         scoring.score(self.user_lost.user, ChallengeGame, 'chall-lost',
             external_id=self.id, points=self.user_lost.points, points2=self.user_lost.points)
 
-        # send activty signal to the loser
-        signal_msg = ugettext_noop('Challenge to {user_to} from {user_from} has expired and it was automatically settled.'\
-            ' {user_lost} lost.')
-
-        signals.addActivity.send(sender=None, user_from=exp_user, \
-                                     user_to=exp_user, \
-                                     message=signal_msg, \
-                                     arguments=dict(user_to=self.user_to, user_from=self.user_from, user_lost=self.user_lost), \
-                                     game=ChallengeGame.get_instance())
         # send activty signal to the winner
         signal_msg = ugettext_noop('Challenge to {user_to} from {user_from} has expired and it was automatically settled.'\
             ' {user_won} won.')
 
-        signals.addActivity.send(sender=None, user_from=self.user_won.user, \
+        signals.addActivity.send(sender=None, user_from=exp_user, \
                                      user_to=self.user_won.user, \
                                      message=signal_msg, \
                                      arguments=dict(user_to=self.user_to, user_from=self.user_from, user_won=self.user_won), \
