f3a13a7c173198f064499b61e3cd34e0b3718d17
Alex Eftimie <alex@eftimie.ro>
2011-09-11 12:08:43 +0300

    [core/artifacts] improve rendering, for user with amounts

--- wouso/core/artifacts/templatetags/artifacts.py
+++ wouso/core/artifacts/templatetags/artifacts.py
@@ -1,15 +1,30 @@
 import django
+from wouso.core.user.models import PlayerArtifactAmount
 
 register = django.template.Library()
 
 @register.simple_tag
 def artifact(artifact):
-    path = artifact.path if artifact else ' '
-    title = artifact.title if artifact else '(no title)'
-    if path[0] == '/':
-        return '<img class="artifact" src="%s" alt="%s" title="%s" />' % (path, title, title)
+    if not artifact:
+        return '(no artifact)'
 
-    return '<div class="artifact artifact-%s" title="%s"></div>' % (path, title)
+    if isinstance(artifact, PlayerArtifactAmount):
+        amount = artifact.amount
+        artifact = artifact.artifact
+    else:
+        amount = None
+    path = artifact.path
+    title = artifact.title
+
+    if path[0] == '/':
+        html = '<img class="artifact" src="%s" alt="%s" title="%s" />' % (path, title, title)
+    else:
+        html = '<div class="artifact artifact-%s" title="%s"></div>' % (path, title)
+
+    if amount is not None:
+        return '<span class="artifact-container">%s<span class="sup">%d</span></span>' % (html, amount)
+    else:
+        return html
 
 @register.simple_tag
 def artifact_full(artif):
