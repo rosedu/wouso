0fcd7c3e9213c35062f2c0d914e4b3e54e47b1d8
Alex Eftimie <alex@eftimie.ro>
2011-09-30 13:23:57 +0300

    [interface/top] exclude superusers from top display

--- wouso/interface/top/models.py
+++ wouso/interface/top/models.py
@@ -101,7 +101,7 @@ class Top(App):
 
     @classmethod
     def get_sidebar_widget(kls, request):
-        top5 = TopUser.objects.all().order_by('-points')[:5]
+        top5 = TopUser.objects.exclude(user__is_superuser=True).order_by('-points')[:5]
         is_top = request.get_full_path().startswith('/top/')
         return render_string('top/sidebar.html',
             {'topusers': top5, 'is_top': is_top, 'top': Top}
--- wouso/interface/top/views.py
+++ wouso/interface/top/views.py
@@ -28,13 +28,14 @@ def gettop(request, toptype=0, sortcrit=0, page=1):
     if pageno < 0:
         raise Http404
 
-    allUsers = TopUser.objects.order_by('-points') #[(pageno-1)*PERPAGE:pageno*PERPAGE]
+    base_query = TopUser.objects.exclude(user__is_superuser=True)
+    allUsers = base_query.order_by('-points') #[(pageno-1)*PERPAGE:pageno*PERPAGE]
     if (allUsers.count() == 0):
         raise Http404
     #if sortcritno == 1:
     #    allUsers = sorted(TopUser.objects.all(), key = lambda p: p.progress, reverse=True)[(pageno-1)*PERPAGE:pageno*PERPAGE]
     if sortcritno == 2:
-        allUsers = TopUser.objects.order_by('-last_seen') #[(pageno-1)*PERPAGE:pageno*PERPAGE]
+        allUsers = base_query.order_by('-last_seen') #[(pageno-1)*PERPAGE:pageno*PERPAGE]
 
     paginator = Paginator(allUsers, PERPAGE)
     try:
