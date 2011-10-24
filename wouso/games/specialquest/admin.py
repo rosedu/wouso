348e4e317700fc81026c845843297ac353e224b1
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-10-24 23:12:45 +0300

    [squest] django-admin drop inline users in group

--- wouso/games/specialquest/admin.py
+++ wouso/games/specialquest/admin.py
@@ -2,13 +2,7 @@ from django.contrib import admin
 from models import SpecialQuestTask, SpecialQuestUser, Invitation, SpecialQuestGroup
 
 
-class SpecialQuestUserInline(admin.TabularInline):
-    model = SpecialQuestUser
-
-class SpecialQuestGroupAdmin(admin.ModelAdmin):
-    inlines = [SpecialQuestUserInline]
-
 admin.site.register(SpecialQuestUser)
 admin.site.register(SpecialQuestTask)
-admin.site.register(SpecialQuestGroup, SpecialQuestGroupAdmin)
+admin.site.register(SpecialQuestGroup)
 admin.site.register(Invitation)
