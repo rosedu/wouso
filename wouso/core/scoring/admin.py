8fe2dc41d550f88a4091687c84c74325b6b4de7d
Alex Eftimie <alex@rosedu.org>
2011-10-24 16:20:06 +0300

    [admin] formula columns

--- wouso/core/scoring/admin.py
+++ wouso/core/scoring/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from wouso.core.scoring.models import *
 
+class FormulaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'formula', 'owner', 'description')
+
 admin.site.register(Coin)
-admin.site.register(Formula)
+admin.site.register(Formula, FormulaAdmin)
 admin.site.register(History)
