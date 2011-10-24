c554b557c587e8199e6d5ac6a3b4345fab7855eb
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-10-24 22:11:07 +0300

    [squest] fix users inline in groups admin

--- wouso/games/specialquest/admin.py
+++ wouso/games/specialquest/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from models import SpecialQuestTask, SpecialQuestUser, Invitation, SpecialQuestGroup
 
-admin.site.register(SpecialQuestUser)
-admin.site.register(SpecialQuestTask)
-admin.site.register(SpecialQuestGroup)
-admin.site.register(Invitation)
 
 class SpecialQuestUserInline(admin.TabularInline):
     model = SpecialQuestUser
 
-class SpecialQuestGroupAdmin(admin.modelAdmin):
-    inlines = [
-        SpecialQuestUserInline,
-    ]
+class SpecialQuestGroupAdmin(admin.ModelAdmin):
+    inlines = [SpecialQuestUserInline]
+
+admin.site.register(SpecialQuestUser)
+admin.site.register(SpecialQuestTask)
+admin.site.register(SpecialQuestGroup, SpecialQuestGroupAdmin)
+admin.site.register(Invitation)
