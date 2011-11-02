7d84f9078f9a95a94642fedc171f46bbb0f44bc5
Alex Eftimie <alex@rosedu.org>
2011-11-02 22:14:00 +0200

    [games/quest] prettify admin

--- wouso/games/quest/admin.py
+++ wouso/games/quest/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from models import Quest, QuestUser
 
-# should filter questions:
-# return get_questions_with_tags('quest', 'quest-%d' % self.id, 'all')
+class QUAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'current_quest', 'current_level', 'started_time', 'finished_time')
+    list_filter = ('current_quest', 'current_level', 'finished')
+
 admin.site.register(Quest)
 
-admin.site.register(QuestUser)
+admin.site.register(QuestUser, QUAdmin)
