d8820cdf6dde5565ea5b02ae9c4878dbb4c2e674
Iulian <iulianstana@yahoo.com>
2011-12-10 09:20:37 +0200

    Modellinge the grandchallenge

--- wouso/games/grandchallenge/models.py
+++ wouso/games/grandchallenge/models.py
@@ -1,13 +1,22 @@
+import logging
 from django.db import models
 from wouso.core.game.models import Game
 from wouso.core.user.models import Player
-from wouso.games.challenge.models import Challenge
 
+from wouso.games.challenge.models import Challenge
+from wouso.interface.top.models import TopUser
+from random import randint
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Q, Max
+from django.utils.translation import ugettext_noop, ugettext as _
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class GrandChallengeUser(Player):
     """ Extension of the user profile for GrandChallenge """
-    lost = models.IntegerField(default="0");
+    lost = models.IntegerField(default="0")
 
     @classmethod
     def eligible(cls, result):
@@ -30,9 +39,20 @@ class GrandChallenge(Challenge):
         self.won, self.lost = None, None
         self.active = True
 
-    def get_challenges(self):
-        return self.ALL
 
+
+    @classmethod
+    def get_challenges(cls):
+        return cls.ALL
+        
+    @classmethod
+    def active(cls):
+        return filter(lambda c: c.active, cls.ALL)        
+
+
+    #Din TopUser faci .user => usr = u.user
+    #usr.get_profile().get_extenion(..)
+    
     def play(self):
         winner = randint(0, 1) == 0 #trebuie generat de joc
         #print " - chall between %s and %s: " % (self.user_from, self.user_to),
@@ -43,7 +63,7 @@ class GrandChallenge(Challenge):
             self.won = self.user_to
             self.user_from.lost += 1
         self.active = False
-        #print self.won, " won"
+        print self.won, " won"
 
 class GrandChallengeGame(Game):
     """ Each game must extend Game """
@@ -60,13 +80,17 @@ class GrandChallengeGame(Game):
         self._meta.get_field('url').default = "grandchallenge_index_view" # aici
         super(GrandChallengeGame, self).__init__(*args, **kwargs)
 
-    def start(self, user_list):
-        assert user_list == NUM_USERS
+    @staticmethod
+    def start(allUsers):
+        user_list = allUsers
         last = None
         for user in user_list:
-            u = user.get_extension(GrandChallengeUser)
+            u = user.user
+            u = u.get_profile().get_extension(GrandChallengeUser)
+                      
             if last is None:
                 last = u
             else:
                 GrandChallenge(u, last)
                 last = None
+
