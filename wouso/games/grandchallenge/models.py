361655432b4b54ba0bfbfc8886604bcd619ed1d6
Iulian <iulianstana@yahoo.com>
2011-12-15 23:48:19 +0200

    grandchallenge

--- wouso/games/grandchallenge/models.py
+++ wouso/games/grandchallenge/models.py
@@ -7,11 +7,6 @@ from wouso.games.challenge.models import Challenge
 from wouso.interface.top.models import TopUser
 from random import randint
 
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models import Q, Max
-from django.utils.translation import ugettext_noop, ugettext as _
-from django.core.urlresolvers import reverse
 
 # Create your models here.
 class GrandChallengeUser(Player):
@@ -20,13 +15,18 @@ class GrandChallengeUser(Player):
 
        
 
-class GrandChallenge(Challenge):
+class GrandChallenge(models.Model):
+   	
+    ALL = []
+    OUT_PLAY = []
+
     def __init__(self, user_from, user_to):
         self.user_from = user_from
         self.user_to = user_to
         self.__class__.ALL.append(self)
         self.won, self.lost = None, None
         self.active = True
+        self.round_number = None
 
 
     @classmethod
@@ -41,18 +41,21 @@ class GrandChallenge(Challenge):
     #Din TopUser faci .user => usr = u.user
     #usr.get_profile().get_extenion(..)
     
-    def play(self):
+    def play(self, round_number):
         winner = randint(0, 1) == 0 #trebuie generat de joc
-        #print " - chall between %s and %s: " % (self.user_from, self.user_to),
+        print " - chall between %s and %s: " % (self.user_from, self.user_to),
         if winner:
             self.won = self.user_from
+            self.lost = self.user_to
             self.user_to.lost += 1
         else:
             self.won = self.user_to
+            self.lost = self.user_from
             self.user_from.lost += 1
         self.active = False
-        print self.won, " won"
-     
+        print "won = %s, lost = %s " % (self.won, self.lost)
+        self.round_number = round_number       
+            
     @classmethod       
     def played_with(cls, user):
         ret = []
@@ -63,18 +66,28 @@ class GrandChallenge(Challenge):
                 ret.append(c.user_from)
         return ret
          
+    @classmethod 
+    def joaca(cls):
+        for c in GrandChallenge.active():
+            #numarul rundei...
+            c.play(1)
+            if(c.lost.lost == 2):
+                cls.OUT_PLAY.append(c.lost)
+                print c.lost
+ 
+    
+    @classmethod 
+    def clasament(cls):
+        arb_win  = GrandChallengeGame.eligible(0)
+        arb_lose = GrandChallengeGame.eligible(1)
+        if(len(arb_win) == 1):
+            cls.OUT_PLAY.append(arb_win[0]) 
+        if(len(arb_lose) == 1):
+            cls.OUT_PLAY.append(arb_lose[0])        
+        return cls.OUT_PLAY[13:16]
         
     @classmethod
     def play_round(cls, l_w):
-    # joc toate provocarile active
-    #print Challenge.all;
-    
-        for c in GrandChallenge.active():
-            c.play()
-    
-    # pentru jucatorii care nu au pierdut de 2 ori
-    # generez noi provocari
-    #Iulian
         if (l_w == 0):
             all = GrandChallengeGame.eligible(0)
         elif(l_w == 1): 
@@ -85,25 +98,26 @@ class GrandChallenge(Challenge):
         while len(all):
         
             u = all[0]
-	# print "elig", u
+	    # print "elig", u
         # nu e respectata# gasesc un jucator cu care nu am mai jucat 
         # si care are acelasi numar de pierdute
         # Iulian
-            played = GrandChallenge.played_with(u)
-            numbLosser = len(GrandChallengeGame.eligible(1))        
+            played = GrandChallenge.played_with(u)    
 	        
-            efm = [eu for eu in all if ((eu.lost == u.lost) and (eu != u) and ( (eu not in played) or (numbLosser == 2)) )]
+            efm = [eu for eu in all if ((eu.lost == u.lost) and (eu != u) and ( (eu not in played) or (eu == all[len(all) - 1])) )]
         
-        #print efm
             if not len(efm):
                 break
-        #print "efm", efm
+
             try:
                 adversar = efm[0]
                 all.remove(adversar)
                 all.remove(u)
                 GrandChallenge(u, adversar)
             except: pass
+
+
+
 
 class GrandChallengeGame(Game):
     """ Each game must extend Game """
@@ -122,13 +136,15 @@ class GrandChallengeGame(Game):
         # Set parent's fields
         self._meta.get_field('verbose_name').default = "GrandChallenges"
         self._meta.get_field('short_name').default = ""
-        # the url field takes as value only a named url from module's urls.py
-        self._meta.get_field('url').default = "grandchallenge_index_view" # aici
+        self._meta.get_field('url').default = "grandchallenge_index_view"
         super(GrandChallengeGame, self).__init__(*args, **kwargs)
 
+
+
+    #schimbat!!!!!!!
     @staticmethod
-    def start(allUsers):
-        user_list = allUsers
+    def start():
+        user_list = GrandChallengeGame.allUsers
         last = None
         for user in user_list:
             u = user.user
@@ -144,4 +160,28 @@ class GrandChallengeGame(Game):
     def eligible(cls, result):
         return filter(lambda user: user.lost==result, cls.ALL)
     
+    @classmethod
+    def is_final(cls):
+        arb_win  = cls.eligible(0)
+        arb_lose = cls.eligible(1)
+        if((len(arb_win) == 1) and (len(arb_lose) == 1)):
+            return True
+        return False
     
+    @classmethod
+    def final_round(cls):
+        arb_win  = cls.eligible(0)
+        arb_lose = cls.eligible(1)
+        GrandChallenge(arb_win[0], arb_lose[0])
+
+    @classmethod
+    def final_second_round(cls):
+        GrandChallenge.play_round(1)
+        
+    @classmethod
+    def is_winner(cls):
+        arb_win  = cls.eligible(0)
+        arb_lose = cls.eligible(1)
+        if((len(arb_win) == 0) and (len(arb_lose) == 2)):
+            return False
+        return True
