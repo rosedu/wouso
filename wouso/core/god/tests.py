c7302aea66879b0f120ba92f6220f73de5f23b5c
Alex Eftimie <alex@rosedu.org>
2012-05-27 11:57:27 +0300

    Refactor tests, move player to setup.

--- wouso/core/god/tests.py
+++ wouso/core/god/tests.py
@@ -10,22 +10,22 @@ __author__ = 'alex'
 
 class GodTestCase(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create(username='testgod')
+        self.player = self.user.get_profile()
+
     def test_others_are_not_elligible_for_challenge(self):
-        user, new = User.objects.get_or_create(username='33')
-        game = ChallengeGame
         god = get_god()
         group, new = Group.objects.get_or_create(name="Others")
         others, new = PlayerGroup.objects.get_or_create(name="Others", group=group)
-        player = user.get_profile()
-        player.groups.add(others)
-        self.assertFalse(god.user_is_eligible(player, game))
+
+        self.player.groups.add(others)
+        self.assertFalse(god.user_is_eligible(self.player, ChallengeGame))
 
     def test_users_are_elligible_for_challenge(self):
-        user, new = User.objects.get_or_create(username='33')
-        game = ChallengeGame
         god = get_god()
-        player = user.get_profile()
-        self.assertTrue(god.user_is_eligible(player, game))
+        self.assertTrue(god.user_is_eligible(self.player, ChallengeGame))
+
 
 
 
