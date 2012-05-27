d87888ddb974c4b474833df95438dcdd262dc086
Alex Morega <alex@grep.ro>
2012-05-27 15:25:42 +0300

    decorator to deprecate a function

--- wouso/core/__init__.py
+++ wouso/core/__init__.py
@@ -1,3 +1,22 @@
+import warnings
+from functools import wraps
 import logging
 
 logger = logging.getLogger('core')
+
+
+def deprecated(message):
+    """ Mark function as deprecated.
+
+        >>> @deprecated("thenineties is deprecated")
+        ... def thenineties():
+        ...     print 'hi'
+
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            warnings.warn(message, DeprecationWarning, stacklevel=2)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
