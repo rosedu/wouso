bd42f0aea6fccd4270aa3d5194fd10d10f99fa8d
iulian <iulianstana@yahoo.com>
2012-07-01 13:00:22 +0300

    Lista de user cu multiple select si butoane de get Profile si get Mesaje

--- wouso/interface/chat/urls.py
+++ wouso/interface/chat/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 urlpatterns = patterns('',
     (r'^$','wouso.interface.chat.views.index'),
     (r'^m/$','wouso.interface.chat.views.sendmessage'),
-    (r'^last/$','wouso.interface.chat.views.online_players')
-
+    (r'^last/$','wouso.interface.chat.views.online_players'),
+    (r'^log/$','wouso.interface.chat.views.log_request')
 )
 
