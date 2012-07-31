b2d8a81bf1b52aea2c1880cd4f69fea98377f3ee
iulianstana <iulianstana@yahoo.com>
2012-07-31 22:41:36 +0300

    [unittest]  Made some testes in chat/tests.py: - test_message_send_manual   : it is working - test_message_send_ulr              : a little bug Solved some bug from cpanel/teste.py: ./manage.py test is working with now errors.

--- wouso/interface/chat/tests.py
+++ wouso/interface/chat/tests.py
@@ -4,8 +4,49 @@ unittest). These will both pass when you run "manage.py test".
 
 Replace these with more appropriate tests for your application.
 """
-
+import unittest
+import django.test
 from django.test import TestCase
+from django.test.client import Client
+from wouso.interface.chat.views import *
+from wouso.interface.chat.models import *
+from wouso.core.user.models import User
+from datetime import datetime, timedelta
+
+
+class ChatTestCase(django.test.TestCase):
+    def setUp(self):
+        self.user, new= User.objects.get_or_create(username='_chat1')
+        self.user.set_password('secret')
+        self.user.save()
+        self.chat_user = self.user.get_profile().get_extension(ChatUser)
+        self.client = Client()
+        self.client.login(username='_chat1', password='secret')
+
+
+    def create_message(self, message):
+        return self.client.post("/chat/m", message)
+
+    def test_message_send_manual(self):
+        room= roomexist('global')
+        timeStamp = datetime.now()
+
+        len_now = len(ChatMessage.objects.all())
+
+        msg = ChatMessage(content='sss', author=self.chat_user, destRoom=room, timeStamp=timeStamp)
+        msg.save()
+
+        len_after = len(ChatMessage.objects.all())
+
+        self.assertEqual(len_now, len_after - 1)
+
+    def test_message_send_url(self):
+        Room = roomexist('global')
+        len_now = len(ChatMessage.objects.filter(destRoom=Room))
+        resp = self.client.post('/chat/m/', {'opcode':'message','room':'global','msg':'salut'})
+        len_after = len(ChatMessage.objects.filter(destRoom=Room))
+
+        self.assertEqual(len_now, len_after - 1)
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
