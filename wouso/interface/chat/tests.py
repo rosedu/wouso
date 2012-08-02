c2a7051863138f63da4abbe43ab64deabc9ee660
iulianstana <iulianstana@yahoo.com>
2012-08-02 15:20:03 +0300

    [unittest] Removed methods for url query. Explain the query purpose and tell who is the message author

--- wouso/interface/chat/tests.py
+++ wouso/interface/chat/tests.py
@@ -6,13 +6,21 @@ Replace these with more appropriate tests for your application.
 """
 import django.test
 from django.test.client import Client
-from wouso.interface.chat.views import *
-from wouso.interface.chat.models import *
+from wouso.interface.chat.views import roomexist
+from wouso.interface.chat.models import ChatUser, ChatRoom, ChatMessage
 from wouso.core.user.models import User
 from datetime import datetime
 import json
 
 class ChatTestCase(django.test.TestCase):
+    """
+        After setUp method is passed, you are login with "_chat1" account
+        Author for all next messages will be "_chat1", the login user
+        There exist 3 types of query for /chat/m url:
+            - message, that send to a specific room a specific message
+            - keepAlive, that make the user a global user
+            - getRoom, that create or giving an existing room for 2 persons.
+    """
     def setUp(self):
         self.user, new= User.objects.get_or_create(username='_chat1')
         self.user.set_password('secret')
@@ -32,52 +40,36 @@ class ChatTestCase(django.test.TestCase):
         self.client = Client()
         self.client.login(username='_chat1', password='secret')
 
+
     def test_message_send_manual(self):
         room= roomexist('global')
         timeStamp = datetime.now()
 
         len_now = len(ChatMessage.objects.all())
-
         msg = ChatMessage(content='sss', author=self.chat_user, destRoom=room, timeStamp=timeStamp)
         msg.save()
-
         len_after = len(ChatMessage.objects.all())
 
         self.assertEqual(len_now, len_after - 1)
 
-
-    def create_message(self, input_msg, room):
-        data = {'opcode':'message','room': room,'msg': input_msg}
-        return self.client.post('/chat/m/', data)
-
-
-    def create_getRoom(self, myID, sendID):
-        data = {'opcode':'getRoom', 'from':myID, 'to':sendID}
-        return self.client.post('/chat/m/', data)
-
-
-    def create_keepAlive(self):
-        return self.client.post('/chat/m/', {'opcode':'keepAlive'})
-
     def test_message_send_url(self):
         len_now = len(ChatMessage.objects.all())
-        message_content = 'salut'
-        self.create_message(message_content, 'global')
+        self.client.post("/chat/m/", {'opcode':'message','room': 'global','msg': 'salut'})
         len_after = len(ChatMessage.objects.all())
 
         last_mess = ChatMessage.objects.all()
         self.assertEqual(len_now, len_after - 1)
-        self.assertEqual(last_mess[0].content, message_content)
+        self.assertEqual(last_mess[0].content, 'salut')
 
 
     def test_message_send_url_private(self):
-        resp = self.create_getRoom(self.user.id, self.user1.id)
+        resp = self.client.post('/chat/m/', {'opcode':'getRoom', 'from':self.user.id, 'to':self.user1.id})
         room = json.loads(resp.content)
         room_name = str(self.user.id) + '-' + str(self.user1.id)
 
-        self.create_keepAlive()
-        self.create_message("Buna", room['name'])
-        self.create_message("Salut", 'global')
+        self.client.post('/chat/m/', {'opcode':'keepAlive'})
+        self.client.post("/chat/m/", {'opcode':'message','room': room['name'],'msg': 'Buna'})
+        self.client.post("/chat/m/", {'opcode':'message','room': 'global'    ,'msg': 'Salut'})
 
         self.assertEqual(room['name'], room_name)
         self.assertEqual(len(ChatMessage.objects.filter(destRoom__name=room['name'], destRoom__participants=self.user)), 1)
@@ -89,11 +81,11 @@ class ChatTestCase(django.test.TestCase):
 
     def test_message_send_more_message(self):
 
-        self.create_keepAlive()
+        self.client.post('/chat/m/', {'opcode':'keepAlive'})
 
         for i in range(10):
             message_content = 'salut' + str(i)
-            self.create_message(message_content, 'global')
+            self.client.post("/chat/m/", {'opcode':'message','room': 'global','msg': message_content})
 
         last_mess = ChatMessage.objects.all()
         for i in range(10):
