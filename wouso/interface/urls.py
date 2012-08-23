9636626e93d25bc945d1c4a6ed6358e29fa92b81
iulianstana <iulianstana@yahoo.com>
2012-08-23 20:47:13 +0300

    [staff] url -paginator problem

--- wouso/interface/urls.py
+++ wouso/interface/urls.py
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
     (r'^user/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
     url(r'^player/(?P<id>\d*)/$', 'wouso.interface.profile.views.user_profile', name='player_profile'),
-    (r'^player/(?P<id>\d*)/(?P<page>\d*)/$', 'wouso.interface.profile.views.user_profile'),
+    url(r'^player/(?P<id>\d*)/(?P<page>\d*)/$', 'wouso.interface.profile.views.user_profile', name="player_profile"),
     url(r'^player/(?P<id>\d*)/points-summary/$', 'wouso.interface.profile.views.player_points_history', name='player_points_history'),
     url(r'^player/cast/to-(?P<destination>\d+)/$', 'wouso.interface.profile.views.magic_cast', name='magic_cast'),
     (r'^player/cast/(?P<destination>\d+)/(?P<spell>\d+)/$', 'wouso.interface.profile.views.magic_cast'),
