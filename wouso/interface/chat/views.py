7b31a1bbe0bd0381f1c3b5eb83eefed7119b52a3
iulianstana <iulianstana@yahoo.com>
2012-08-31 13:44:24 +0300

    [contact-box] appear bug solved

--- wouso/interface/chat/views.py
+++ wouso/interface/chat/views.py
@@ -48,9 +48,10 @@ def add_message(text, sender, toRoom):
     diff = timeStamp - sender.lastMessageTS
 
     #TODO: Putem renunta la spam:) este inutil.
-    difference_in_seconds = 1;
-    #difference_in_seconds = (diff.microseconds + (diff.seconds + diff.days * 24 * 3600) * 10**6) / 10**6
+    #difference_in_seconds = 1;
+    difference_in_seconds = (diff.microseconds + (diff.seconds + diff.days * 24 * 3600) * 10**6) / 10**6
     #if diff.total_seconds() > 0.5:
+    print text + "  "
     if sender.has_modifier('block-communication'):
         return False
     if sender.has_modifier('change-messages'):
@@ -144,7 +145,7 @@ def log_request(request):
 def online_players(request):
 
     # gather users online in the last ten minutes
-    oldest = datetime.now() - timedelta(hours = 1000)
+    oldest = datetime.now() - timedelta(minutes = 10)
     online_last10 = Player.objects.filter(last_seen__gte=oldest).order_by('user__username')
 
     def is_not_blocked(x):
