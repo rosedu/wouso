8f700da8d0420f3b38d27e8a6e01e0fb8d3fab64
Keops <keops@keops-HP-635-Notebook-PC.(none)>
2012-08-31 21:21:30 +0300

    Solved add-player issue resolved

--- wouso/interface/cpanel/views.py
+++ wouso/interface/cpanel/views.py
@@ -575,8 +575,9 @@ def players(request):
 def add_player(request):
     form = UserForm()
     if request.method == "POST":
-        user = UserForm(data = request.POST)
+        user = UserForm(data = request.POST)       
         if user.is_valid():
+            user.instance.set_password(request.POST['password'])
             user.save()
             return HttpResponseRedirect(reverse('wouso.interface.cpanel.views.players'))
         else:
