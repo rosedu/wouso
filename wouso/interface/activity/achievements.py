d8c46558e013ceee96bab43375eaaafb43f46e5a
Mihai <mihai@Brasi.(none)>
2012-09-18 13:46:14 +0300

    Added 30 Challenges played and 10 won Challenges in a row achievement, not tested

--- wouso/interface/activity/achievements.py
+++ wouso/interface/activity/achievements.py
@@ -18,6 +18,27 @@ def consecutive_seens(player, timestamp):
 
     return len(activities)
 
+def challenge_count(palyer):
+    """
+     Return the count of challenges played by player.
+    """
+    games_played = Activity.get_private_activity(player).filter(action__contains='chall').count()
+
+    return games_played;
+
+def consecutive_chall_won(player):
+    """
+     Return the count of won challenges in a row
+    """
+    activities = Activity.get_private_activity(player).filter(action__contains='chall').order_by('-timestamp')[:12]
+    result = 0;
+    for i in activities:
+        if i.user_from == player:
+            result += 1
+        else:
+            return result
+    return result
+
 class Achievements(App):
 
     @classmethod
@@ -36,8 +57,19 @@ class Achievements(App):
         action = kwargs.get('action', None)
         player = kwargs.get('user_from', None)
         if not action:
-            # Ignore signals without an action.
             return
+
+        if 'chall' in action:
+            # Check if number of challenge games is 30*k
+            games_played = challenge_count(player)
+            if games_played > 0 and mod(games_played, 30) == 0:
+                if not player.magic.has_modifier('ach-chall-30'):
+                    cls.earn_achievement(player, 'ach-chall-30');
+
+            #Check 10 won challenge games in a row
+            if consecutive_chall_won(player) >= 10:
+                if not player.magic.has_modifier('ach-chall-won-10'):
+                    cls.earn_achievement(player, 'ach-chall-won-10');
 
         if action == 'seen':
             # Check previous 10 seens
@@ -47,7 +79,7 @@ class Achievements(App):
 
     @classmethod
     def get_modifiers(self):
-        return ['ach-login-10']
+        return ['ach-login-10', 'ach-chall-30', 'ach-chall-won-10']
 
 def check_for_achievements(sender, **kwargs):
     Achievements.activity_handler(sender, **kwargs)
