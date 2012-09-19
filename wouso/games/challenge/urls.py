0b8846217a390a1f0edb12f4a3b76c7eb6772d85
Vicentiu Ciorbaru <cvicentiu@gmail.com>
2012-09-19 05:19:41 -0700

    [games] Challenge: Added urls to link to the statistics views.

--- wouso/games/challenge/urls.py
+++ wouso/games/challenge/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('wouso.games.challenge.views',
     url(r'^history/(?P<playerid>\d+)/$', 'history', name='chellenge_history'),
 
     url(r'^randomchallenge/$', 'challenge_random', name='challenge_random'),
+	url(r'^stats/$', 'challenge_stats', name='challenge_stats'),
+	url(r'^stats/(?P<target_id>\d+)/', 'detailed_challenge_stats',
+		name='detailed_challenge_stats'),
 )
