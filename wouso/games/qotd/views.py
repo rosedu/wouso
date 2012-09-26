89fec9d4c1ea968f3617557a5d4bf7d68137557d
Keops <keops@keops-HP-635-Notebook-PC.(none)>
2012-09-26 13:31:01 +0300

    Blind Spell

--- wouso/games/qotd/views.py
+++ wouso/games/qotd/views.py
@@ -7,7 +7,6 @@ from django.template import RequestContext
 from django.template.loader import render_to_string
 from models import QotdUser, QotdGame
 from forms import QotdForm
-
 @login_required
 def index(request):
     if QotdGame.disabled():
@@ -15,7 +14,9 @@ def index(request):
 
     profile = request.user.get_profile()
     qotd_user = profile.get_extension(QotdUser)
-
+    
+    if qotd_user.has_modifier('qotd-blind'):
+        return render_to_response('qotd/index.html', {"error":"You have been blinded,you cannot answer to the Question of the Day"}, context_instance=RequestContext(request))
     if not qotd_user.has_question:
         qotd = QotdGame.get_for_today()
         qotd_user.set_question(qotd)
