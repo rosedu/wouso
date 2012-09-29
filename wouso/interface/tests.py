73f9518f9e7fe8dd73f69f3f47f099aad62edd8b
Alex Eftimie <alex@eftimie.ro>
2012-09-29 22:09:26 +0300

    Test if Jenkins auto-builds and sends email when tests fail.

--- wouso/interface/tests.py
+++ wouso/interface/tests.py
@@ -2,11 +2,14 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 class TestInterface(TestCase):
-	def test_homepage_anonymous(self):
-		result = self.client.get('/')
-		self.assertEqual(result.status_code, 200)
-	
-	def test_homepage_player(self):
-		user = User.objects.create_user('test', 'test')
-		self.client.login(username='test', password='test')
-		self.test_homepage_anonymous()
+    def test_homepage_anonymous(self):
+        result = self.client.get('/')
+        self.assertEqual(result.status_code, 200)
+
+    def test_homepage_player(self):
+        user = User.objects.create_user('test', 'test')
+        self.client.login(username='test', password='test')
+        self.test_homepage_anonymous()
+
+    def test_jenkins(self):
+        self.assertTrue(True)
