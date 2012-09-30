042e4352bb7c2ec3231dd99d2b831f586e91e16f
Vicentiu Ciorbaru <cvicentiu@gmail.com>
2012-09-30 22:20:29 +0300

    [core] Security: Added new SecurityConfig model

--- wouso/core/security/models.py
+++ wouso/core/security/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class SecurityConfig(models.Model):
+    """Class that allows enabling / disabling of security rules"""
+
+    app_on_choices = [('chall-won', 'Challenge Won'), ('login', 'Login')]
+
+    id = models.CharField(max_length=100, primary_key=True)
+    applies_on = models.CharField(max_length=100, choices=app_on_choices)
+    description = models.CharField(max_length=100)
+    penalty_value = models.IntegerField(default=5, blank=False)
+    enabled = models.BooleanField(default=True, blank=False)
+
+    def __unicode__(self):
+        return self.id
