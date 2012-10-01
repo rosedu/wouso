9dff2f87245c9fbc2b1fd963bc7dabd5267542ba
Vicentiu Ciorbaru <cvicentiu@gmail.com>
2012-10-01 11:01:39 +0300

    [core] Security: Changed app_on_choices constant to uppercase.

--- wouso/core/security/models.py
+++ wouso/core/security/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class SecurityConfig(models.Model):
     """Class that allows enabling / disabling of security rules"""
 
-    app_on_choices = [('chall-won', 'Challenge Won'), ('login', 'Login')]
+    APP_ON_CHOICES = (('chall-won', 'Challenge Won'), ('login', 'Login'))
 
     id = models.CharField(max_length=100, primary_key=True)
-    applies_on = models.CharField(max_length=100, choices=app_on_choices)
+    applies_on = models.CharField(max_length=100, choices=APP_ON_CHOICES)
     description = models.CharField(max_length=100)
     penalty_value = models.IntegerField(default=5, blank=False)
     enabled = models.BooleanField(default=True, blank=False)
