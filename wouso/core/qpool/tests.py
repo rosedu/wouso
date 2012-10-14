9d101a6258f178a7ceb2a7e71d0daf4908b3338b
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-10-14 18:31:49 +0300

    update qpool test for answer_type instead of type

--- wouso/core/qpool/tests.py
+++ wouso/core/qpool/tests.py
@@ -8,44 +8,44 @@ class QpoolTestCase(TestCase):
         self.user = User.objects.create(username='_test')
         self.user.save()
         self.question = self._get_foo_question()
-        
+
     def tearDown(self):
         self.user.delete()
         self.question.delete()
-        
+
     def _get_foo_question(self):
         q = Question.objects.create(text='Cati ani ai',
             proposed_by=self.user,
             endorsed_by=self.user,
             active=True)
         q.save()
-        answers = [Answer.objects.create(question=q, 
+        answers = [Answer.objects.create(question=q,
             text=str(i),
             correct=False if i != 2 else True) for i in range(4)]
         [a.save() for a in answers]
         return q
-        
+
     def testQuestionAdd(self):
         q = self.question
-        
+
         self.assertTrue(isinstance(q, Question))
         self.assertEqual(q.answers.count(), 4)
         self.assertTrue(q.is_valid())
-    
+
     def testQuestionTag(self):
         tag = Tag.objects.create(name='test')
         tag.save()
         self.question.tags.add(tag)
-        
+
         self.assertTrue(self.question in get_questions_with_tags('test'))
         self.assertTrue(self.question in get_questions_with_tags(['test']))
         self.assertTrue(self.question not in get_questions_with_tags('inexistent'))
-        
+
         # check multiple
         tag2 = Tag.objects.create(name='other')
         tag2.save()
         self.question.tags.add(tag2)
-        
+
         self.assertTrue(self.question in get_questions_with_tags(['test', 'other']))
         self.assertTrue(self.question not in get_questions_with_tags(['test', 'inexistent'], 'all'))
         self.assertTrue(self.question not in get_questions_with_tags(['coco', 'test'], 'all'))
@@ -97,7 +97,7 @@ class QuestionTest(TestCase):
         Answer.objects.create(question=q, correct=True)
         self.assertTrue(q.is_valid())
 
-        q.type = 'F'
+        q.answer_type = 'F'
         q.save()
 
         self.assertFalse(q.text)
