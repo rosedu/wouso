56ec89914b0d7ea88ae43004a2832b1be7fe1b54
Alex Eftimie <alex@rosedu.org>
2012-10-19 16:22:09 +0300

    [refactor] Attempt to fix circular imports, again.

--- wouso/interface/apps/__init__.py
+++ wouso/interface/apps/__init__.py
@@ -1,8 +1,10 @@
-from wouso.core.magic.models import Bazaar
-from wouso.interface.apps.messaging.models import MessageApp
-from wouso.interface.activity.achievements import Achievements
+
 
 def get_apps():
     """ Same as wouso.core.game.get_games
     """
+    from wouso.core.magic.models import Bazaar
+    from wouso.interface.apps.messaging.models import MessageApp
+    from wouso.interface.activity.achievements import Achievements
+
     return MessageApp, Bazaar, Achievements
