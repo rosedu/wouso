733bdd238a27dac9caad3397011f005b843a95f2
Alex Eftimie <alex@rosedu.org>
2012-10-19 18:28:43 +0300

    Fix another test failing in jenkins.

--- wouso/interface/apps/qproposal/models.py
+++ wouso/interface/apps/qproposal/models.py
@@ -1,11 +1,13 @@
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, NoReverseMatch
 from django.utils.translation import ugettext as _
 from wouso.core.app import App
 
 class Qproposal(App):
     @classmethod
     def get_footer_link(kls, request):
-        url = reverse('propose')
-
-        return '<a href="%s">' % url + _('Propose question') + '</a>'
+        try:
+            url = reverse('propose')
+            return '<a href="%s">' % url + _('Propose question') + '</a>'
+        except NoReverseMatch:
+            return '-'
   
