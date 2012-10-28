98d78bd71a5dcec3cef72d8095782e25b67d4f2c
Mihai <mihai@Brasi.(none)>
2012-10-28 22:36:56 +0200

    Added feature to show number of can_play users in cpanel; Fix issue #176

--- wouso/interface/cpanel/views.py
+++ wouso/interface/cpanel/views.py
@@ -53,9 +53,14 @@ def dashboard(request):
     if last_run == "":
         last_run="wousocron was never run"
 
-    #online members
+    # online members
     oldest = datetime.datetime.now() - datetime.timedelta(minutes = 10)
     online_last10 = Player.objects.filter(last_seen__gte=oldest).order_by('-last_seen')
+
+    # number of players which can play
+    players = list(Player.objects.all())
+    cp_players = [p for p in players if p.race and p.race.can_play]
+    cp_number = len(cp_players)
 
     return render_to_response('cpanel/index.html',
                               {'nr_future_questions' : nr_future_questions,
@@ -69,6 +74,7 @@ def dashboard(request):
                                'staff': staff_group,
                                'last_run': last_run,
                                'online_users': online_last10,
+                               'cp_number': cp_number,
                                },
                               context_instance=RequestContext(request))
 
