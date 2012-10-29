d084777f9253b441eb5233653326f6de63b5d0ce
Mihai <mihai@Brasi.(none)>
2012-10-29 02:37:39 +0200

    Added 5 spells casted on a player achievement

--- wouso/interface/activity/tests.py
+++ wouso/interface/activity/tests.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
-from wouso.core.magic.models import Artifact
+from wouso.core.magic.models import Artifact, Spell
+from wouso.core.magic.manager import MagicManager
 from wouso.core.tests import WousoTest
 from wouso.core import scoring
 from wouso.games.qotd.models import QotdGame
@@ -11,7 +12,7 @@ from achievements import consecutive_chall_won, challenge_count
 from achievements import refused_challenges, get_challenge_time
 from achievements import unique_users_pm , wrong_first_qotd
 from achievements import get_chall_score, challenges_played_today
-from achievements import check_for_god_mode
+from achievements import check_for_god_mode, spell_count
 from models import Activity
 from achievements import Achievements
 from . import signals
@@ -528,8 +529,33 @@ class WinFastTest(WousoTest):
         self.assertTrue(player.magic.has_modifier('ach-win-fast'))
 
 
+class SpellAchievement(WousoTest):
+
+    def test_spell_count(self):
+        player = self._get_player()
+        spell = Spell.objects.create(name="test", title="", description="",
+                image=None, percents=100, type='s')
+        player.magic.add_spell(spell)
+        player.magic.cast_spell(spell, player, datetime.now() + timedelta(days=3))
+        self.assertTrue(player.magic.is_spelled)
+        self.assertTrue(spell_count(player), 1)
+
+
+    def test_spell_count_activity(self):
+        Artifact.objects.create(group=Artifact.DEFAULT(), name='ach-spell-5')
+        player = self._get_player()
+        for i in range(1, 6):
+            name = "test" + str(i)
+            spell = Spell.objects.create(name=name, title="", description="",
+                    image=None, percents=100)
+            player.magic.add_spell(spell)
+            player.magic.cast_spell(spell, player, datetime.now() + timedelta(days=i))
+        signals.addActivity.send(sender=None, user_from=player,
+                user_to=player, action="cast", game=None)
+        self.assertTrue(player.magic.has_modifier('ach-spell-5'))
+
 class GodModeTest(WousoTest):
-    
+
     def test_check_for_god_mode1(self):
         player=self._get_player()
         timestamp=datetime.now()
@@ -537,7 +563,7 @@ class GodModeTest(WousoTest):
             timestamp -= timedelta(days=1)
             Activity.objects.create(timestamp=timestamp, user_from=player, user_to=player, action='qotd-correct')
         self.assertTrue(check_for_god_mode(player,5,0))
-    
+
     def test_check_for_god_mode2(self):
         player=self._get_player()
         timestamp=datetime.now()
@@ -548,7 +574,7 @@ class GodModeTest(WousoTest):
                 continue
             Activity.objects.create(timestamp=timestamp, user_from=player, user_to=player, action='qotd-correct')
         self.assertFalse(check_for_god_mode(player,5,0))
-        
+
     def test_check_for_god_mode3(self):
         player = self._get_player()
         player2 = self._get_player(1)
@@ -558,16 +584,15 @@ class GodModeTest(WousoTest):
             Activity.objects.create(timestamp=timestamp, user_from=player, user_to=player2, action='chall-won')
             Activity.objects.create(timestamp=timestamp, user_from=player, user_to=player, action='qotd-correct')
         self.assertTrue(check_for_god_mode(player,5,5))
-        
+
         Artifact.objects.create(group=Artifact.DEFAULT(), name='ach-god-mode-on')
         signals.addActivity.send(sender=None, user_from=player,
                                      user_to=player,
                                      action='seen',
                                      game=None)
         self.assertTrue(player.magic.has_modifier('ach-god-mode-on'))
-        
-        
-    
+
+
     def test_check_for_god_mode4(self):
         player = self._get_player()
         player2 = self._get_player(1)
