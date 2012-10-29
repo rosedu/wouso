8231ca8bac56d246bbc43da02898c82d9450ad15
Alex Eftimie <alex@rosedu.org>
2012-10-30 00:02:18 +0200

    [scoring] Forgotten string format

--- wouso/core/scoring/sm.py
+++ wouso/core/scoring/sm.py
@@ -136,7 +136,7 @@ def update_points(player, game):
             rollback(player, game, 'level-gold', external_id=player.level_no, level=player.level_no)
             signals.addActivity.send(sender=None, user_from=player,
                                 user_to=player, message=signal_msg,
-                                arguments=dict(level=level),
+                                arguments=dict(level=level, amount=amount),
                                 game=game)
         else:
 
