98e039fbadb9868e290a446c8db4161029771e5d
Keops <keops@keops-HP-635-Notebook-PC.(none)>
2012-10-30 09:52:46 +0200

    Removed redundant view

--- wouso/interface/views.py
+++ wouso/interface/views.py
@@ -23,7 +23,6 @@ from wouso.core.user.models import Player, PlayerGroup
 from wouso.interface.activity.models import Activity
 from wouso.interface.top.models import TopUser, History as TopHistory
 from wouso.interface.activity import signals
-from wouso.core.security.models import Report, add_report
 
 
 def get_wall(page=u'1'):
@@ -251,29 +250,3 @@ def ui(request):
     """
 
     return render_to_response('interface/ui.html', {}, context_instance=RequestContext(request))
-
-@login_required
-def report(request,id):
-
-    if request.user.id == int(id):
-        return homepage(request, error='You cannot report yourself')
-    get_object_or_404(User,pk=id)
-
-    if request.method == "POST":
-        form = UserReportForm(request.POST)
-        if form.is_valid():
-            user_from=request.user
-            user_to = User.objects.get(pk=id)
-            signals.addActivity.send(sender=None,
-                                    user_from=user_from.get_profile().get_extension(Player),
-                                    user_to=user_to.get_profile().get_extension(Player),
-                                    action="report",
-                                    game=None)
-            add_report(user_from=user_from, user_to=user_to, text=request.POST['message'])
-            request.session["report_msg"] = "The report was successfully submitted"
-            return redirect('player_profile', id=id)
-    else:
-        form = UserReportForm()
-    return render_to_response('profile/report_form.html',
-                                {'id': id,'form': form},
-                                context_instance=RequestContext(request))
