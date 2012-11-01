b174ee9ebe8c6fc888c79ca021ecb9b37c296f27
Alex Eftimie <alex@eftimie.ro>
2012-11-01 13:30:12 +0200

    [workshop] debugging -2

--- wouso/games/workshop/admin.py
+++ wouso/games/workshop/admin.py
@@ -7,8 +7,8 @@ admin.site.register(Assessment)
 admin.site.register(Workshop)
 
 class RAdmin(admin.ModelAdmin):
-    list_filter = ('answer__assessment', 'answer')
-    list_display = ('answer', 'reviewer', 'feedback', 'answer_grade', 'review_grade')
+    list_filter = ('answer__assessment', 'answer__assessment__workshop', 'answer', 'reviewer')
+    list_display = ('id', 'workshop', 'reviewer', 'feedback', 'answer_grade', 'review_grade')
 
 admin.site.register(Review, RAdmin)
 admin.site.register(Answer)
