b177ef4e8544f7ecd2611e0e6b1cf3ea3979b570
iulianstana <iulianstana@yahoo.com>
2012-11-04 01:33:46 +0200

    [chat-socketio] You can see the text that was typed after you enter on channel. The contact-box stay in the window.

--- wouso/resources/static/js/contactbox.js
+++ wouso/resources/static/js/contactbox.js
@@ -71,19 +71,19 @@ function on_userlist_mouseover(nickname, name, score, avatar, level, id, x_posit
 
             if (x_position == 1){
                 if(tempX + 220 > window.innerWidth)
-                    position = window.innerWidth - 220;
+                    position = window.innerWidth - 250;
                 else
                     position = tempX;
             }else {
                 var TopLeft = getElementTopLeft("GlobalboxUserList");
                 if(TopLeft.left + 360 > window.innerWidth)
-                    position = window.innerWidth - 220;
+                    position = window.innerWidth - 250;
                 else
                     position = TopLeft.left + 140
             }
 
             if(tempY + 145 > window.innerHeight)
-                tempY = window.innerHeight - 145;
+                tempY = window.innerHeight - 175;
 
             $("#Contactbox").css("top",tempY+"px").css("left",position + "px");
             $("#Contactbox").show();
@@ -106,7 +106,7 @@ function on_userlist_mouseover(nickname, name, score, avatar, level, id, x_posit
 
 function on_userlist_mouseout(x_position) {
     if(x_position == 0){
-        time_for_chat_hide = setTimeout(function(){$("#Contactbox").hide()},500);
+        time_for_chat_hide = setTimeout(function(){$("#Contactbox").hide()},1000);
     }
     else
         time_for_chat_hide = setTimeout(function(){$("#Contactbox").hide();},0);
