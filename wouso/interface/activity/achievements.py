c898449130b8575483dcad1530206e41b3b9e900
Alex Eftimie <alex@eftimie.ro>
2012-11-08 15:52:51 +0200

    [achievements] Fix aweful bug because of a trailing comma. Guys, look at the keyboard when you type.

--- wouso/interface/activity/achievements.py
+++ wouso/interface/activity/achievements.py
@@ -177,7 +177,7 @@ class Achievements(App):
         result = player.magic.give_modifier(modifier)
         if result is not None:
             message = ugettext_noop('earned {artifact}')
-            action_msg = 'earned-ach',
+            action_msg = 'earned-ach'
             addActivity.send(sender=None, user_from=player, game=None, message=message,
                 arguments=dict(artifact=result.artifact), action=action_msg
             )
