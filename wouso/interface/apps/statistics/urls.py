f839e44c38d07c2826d47681ea547c2c893969ca
Alex Eftimie <alex@rosedu.org>
2013-01-08 09:28:10 +0200

    [statistics] Add some pretty numbers about the game, in /stats/extra/

--- wouso/interface/apps/statistics/urls.py
+++ wouso/interface/apps/statistics/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 #from django.conf import settings
 
 urlpatterns = patterns('wouso.interface.apps.statistics.views',
-    url(r'^$', 'stats', name='stats')
+    url(r'^$', 'stats', name='stats'),
+    url(r'^extra/$', 'extra_stats', name='extra_stats'),
 )
