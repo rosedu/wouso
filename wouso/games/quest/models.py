15e3748add6a73d1c11bc86a9ac745c014a493bb
Alex Eftimie <alex@rosedu.org>
2013-01-13 18:43:02 +0200

    [quest] Also bonus the finalists.

--- wouso/games/quest/models.py
+++ wouso/games/quest/models.py
@@ -312,7 +312,7 @@ class FinalQuest(Quest):
         return False
 
     def give_level_bonus(self):
-        for level in xrange(len(self.levels)):
+        for level in xrange(len(self.levels) + 1):
             if level == 0:
                 continue
             users = QuestUser.objects.filter(current_level=level, race__can_play=True)
