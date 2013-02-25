c6abdc09dc9aaa17a22fc4226f222a95851136bd
Alex Eftimie <alex@eftimie.ro>
2013-02-25 16:04:19 +0200

    [core] common: use name for unicode, not id (coin, formula, category)

--- wouso/core/common.py
+++ wouso/core/common.py
@@ -113,6 +113,9 @@ class Item(object):
     def __str__(self):
         return u'%s' % self.id
 
+    def __unicode__(self):
+        return self.name if hasattr(self, 'name') else str(self)
+
 
 class CachedItem(Item):
     """
