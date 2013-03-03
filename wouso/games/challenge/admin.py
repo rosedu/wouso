33f9ff1dfd2737eec532c2c0109c61bea1b006cf
Alex Eftimie <alex@eftimie.ro>
2013-03-03 21:50:22 +0200

    [games][challenge] Show last_launched in admin.

--- wouso/games/challenge/admin.py
+++ wouso/games/challenge/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from wouso.games.challenge.models import Challenge, ChallengeUser, Participant
 
+class ChallengeUserAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'last_launched')
+
 admin.site.register(Challenge)
 admin.site.register(Participant)
-admin.site.register(ChallengeUser)
+admin.site.register(ChallengeUser, ChallengeUserAdmin)
