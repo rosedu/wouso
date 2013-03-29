87abf3e55727c0ca4fa086c3f783a17015d62509
Nicu Badescu <badescunicu@yahoo.com>
2013-03-29 22:01:34 +0200

    Order the challenges by date and put django imports before wouso ones

--- wouso/games/challenge/cpanel_views.py
+++ wouso/games/challenge/cpanel_views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from wouso.core.decorators import staff_required
 from wouso.games.challenge.models import Challenge
-from django.template import RequestContext
 
 @staff_required
 def list_challenges(request):
-    challenges = Challenge.objects.all()
+    challenges = Challenge.objects.all().order_by('-date')
     context = {
        'challenges': challenges,
     }
-        
+    
     return render_to_response('challenge/cpanel/list_challenges.html', context,
                               context_instance=RequestContext(request))
