5c6305db7154077855b7535bec28f977346ce861
Nicu Badescu <badescunicu@yahoo.com>
2013-04-02 10:12:33 +0300

    Add navigation

--- wouso/games/challenge/cpanel_views.py
+++ wouso/games/challenge/cpanel_views.py
@@ -7,5 +7,8 @@ class ListChallenges(ListView):
     paginate_by = 50
     context_object_name = 'challenges'
     template_name = 'challenge/cpanel/list_challenges.html'
+    
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-date')
 
 list_challenges = staff_required(ListChallenges.as_view())
