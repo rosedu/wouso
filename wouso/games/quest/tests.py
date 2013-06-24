6c69a798d1a14df42475958ab6e12d30fcbc21dc
Nicu Badescu <badescunicu@yahoo.com>
2013-06-25 01:18:04 +0300

    broken tests.py

--- wouso/games/quest/tests.py
+++ wouso/games/quest/tests.py
@@ -9,6 +9,35 @@ from wouso.core.scoring import Coin
 from wouso.core.tests import WousoTest
 from wouso.core.user.models import Race
 
+#my test class
+class QuestStatistics(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(username='test1')
+        self.quest_user1 = self.user1.get_profile().get_extension(QuestUser)
+        self.user2 = User.objects.create(username='test2')
+        self.quest_user2 = self.user2.get_profile().get_extension(QuestUser)
+        scoring.setup_scoring()
+
+    def test_check_if_both_players_finished(self):
+         category = Category.add('quest')
+         question = Question.objects.create(text='question1', answer_type='F',
+                                            category=category, active=True)
+         answer = Answer.objects.create(text='first answer', correct=True, question=question)
+ 
+         start = datetime.datetime.now()
+         end = datetime.datetime.now() + timedelta(days=1)
+         quest = Quest.objects.create(start=start, end=end)
+ 
+         quest.questions.add(question)
+ 
+         self.quest_user1.current_quest = quest
+         self.quest_user2.current_quest = quest
+         quest.check_answer(self.quest_user1, 'first answer')
+         quest.check_answer(self.quest_user2, 'first answer')
+         self.assertTrue(self.quest_user1.finished)
+         self.assertTrue(self.quest_user2.finished)
+         
+#old test class
 class QuestTestCase(TestCase):
     def setUp(self):
         self.user, new = User.objects.get_or_create(username='_test')
@@ -16,31 +45,31 @@ class QuestTestCase(TestCase):
         profile = self.user.get_profile()
         self.quest_user = profile.get_extension(QuestUser)
         scoring.setup_scoring()
-
+ 
     def tearDown(self):
         #self.user.delete()
         pass
-
-    def check_answer_test(self):
+ 
+    def test_check_answer(self):
         cat = Category.add('quest')
         question = Question.objects.create(text='test_q', answer_type='F',
                                            category=cat, active=True)
         answer1 = Answer.objects.create(text='test_a1', correct=True, question=question)
         answer2 = Answer.objects.create(text='test_a2', correct=True, question=question)
-
+ 
         start = datetime.datetime.now()
         end = datetime.datetime.now() + timedelta(days=1)
         quest = Quest.objects.create(start=start, end=end)
-
+ 
         quest.questions.add(question)
-
+ 
         self.assertEqual(quest.count, 1)
-
+ 
         self.quest_user.current_quest = quest
         #self.quest_user.current_level = 0
-
+ 
         quest.check_answer(self.quest_user, 'Test_a2')
-
+ 
         self.assertTrue(self.quest_user.finished)
 
 
@@ -80,6 +109,7 @@ class FinalQuestTestCase(WousoTest):
 
         self.assertFalse(final.answer_correct(0, question, u1.user.username + "wrong", u1))
         self.assertTrue(final.answer_correct(0, question, u1.user.username, u1))
+
 
 # API tests
 class QuestAPITestCase(WousoTest):
