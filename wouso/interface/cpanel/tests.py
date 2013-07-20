805eb4c8351fd5545daeba292cfb132fa3819231
Nicu Badescu <badescunicu@yahoo.com>
2013-07-20 10:45:21 +0300

    Add test for edit_formula view (GET)

--- wouso/interface/cpanel/tests.py
+++ wouso/interface/cpanel/tests.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from django.test.client import Client
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from wouso.core.scoring.models import Formula
+from wouso.core.tests import WousoTest
 
 class addPlayerTestCase(TestCase):
     def setUp(self):
@@ -18,3 +21,19 @@ class addPlayerTestCase(TestCase):
 
         self.assertEqual(resp.status_code, 302)
         self.assertEqual(old_number + 1, new_number)
+
+class CpanelViewsTest(WousoTest):
+    def setUp(self):
+        super(CpanelViewsTest, self).setUp()
+        self.admin = self._get_superuser()
+        self.client = Client()
+        self.client.login(username='admin', password='admin')
+
+    def test_edit_formula_view_get(self):
+        formula = Formula.objects.create(name='test_formula1', definition='points=1234',
+                                         description='First formula of the game')
+        response = self.client.get(reverse('edit_formula', args=[formula.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'value="test_formula1"')
+        self.assertContains(response, 'value="points=1234"')
+        self.assertContains(response, 'First formula of the game')
