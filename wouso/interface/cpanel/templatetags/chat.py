851c79dd7e890ea7998e51fce9fcfe05ef93e076
Alex Eftimie <alex@eftimie.ro>
2013-09-27 10:11:43 -0400

    [chat] Better handle default setup.

---
+++ wouso/interface/cpanel/templatetags/chat.py
@@ -0,0 +1,19 @@
+from django import template
+from django.conf import settings
+from django.template.loader import render_to_string
+from wouso.core.config.models import Setting
+
+register = template.Library()
+
+@register.simple_tag
+def chat_scripts():
+    """
+    Dump the chat scris
+    :return:
+    """
+    if settings.CHAT_ENABLED:
+        return render_to_string('chat/setup.html', {'chat_host': Setting.get('chat_host').get_value(),
+                                                    'chat_port': Setting.get('chat_port').get_value(),
+                                                    'basepath': settings.FORCE_SCRIPT_NAME,
+        })
+    return ''
