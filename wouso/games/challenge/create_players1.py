4e293c3dda13f617b77d219db69b85164412207e
cosminBoaca <cosmin_267646@yahoo.com>
2013-10-10 23:54:54 +0300

    Delete create_players1.py

--- wouso/games/challenge/create_players1.py
+++
@@ -1,9 +0,0 @@
-
-from wouso.core.user.models import User
-from wouso.core.user.models import Race
-from wouso.core.user.models import Player
-
-for i in range(1):
-  Player.objects.all().delete()
-
-
