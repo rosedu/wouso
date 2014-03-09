ee0cc9d480512329ec77851c4bedc8b9b0c58b08
Dennis Plosceanu <dennisplosceanu@gmail.com>
2014-03-09 16:50:36 +0200

    CPanel: Update Race/Group forms in forms.py

--- wouso/interface/cpanel/forms.py
+++ wouso/interface/cpanel/forms.py
@@ -161,6 +161,10 @@ class RoleForm(forms.ModelForm):
         model = Group
 
 
+class UpdateRaceForm(forms.ModelForm):
+    class Meta:
+        model = Race
+        exclude = ['artifacts']
 
 
 class RaceForm(forms.ModelForm):
@@ -170,6 +174,11 @@ class RaceForm(forms.ModelForm):
 
 
 class PlayerGroupForm(forms.ModelForm):
+    class Meta:
+        model = PlayerGroup
+        exclude = ['artifacts', 'owner']
+
+class UpdatePlayerGroupForm(forms.ModelForm):
     class Meta:
         model = PlayerGroup
         exclude = ['artifacts', 'owner']
