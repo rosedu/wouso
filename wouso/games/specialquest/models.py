d251ad98524b4159bcc0a38d1fb4b0859ea1c842
Gabriel59 <ivascu.gabriel59@gmail.com>
2014-08-29 20:56:10 +0300

    Change field 'definition' into 'expression' in games directory

--- wouso/games/specialquest/models.py
+++ wouso/games/specialquest/models.py
@@ -152,7 +152,7 @@ class SpecialQuestGame(Game):
     def get_formulas(kls):
         fs = []
         quest_game = kls.get_instance()
-        fs.append(dict(name='specialquest-passed', definition='gold={value}',
+        fs.append(dict(name='specialquest-passed', expression='gold={value}',
             owner=quest_game.game,
             description='Points earned when finishing a task. Arguments: value.')
         )
