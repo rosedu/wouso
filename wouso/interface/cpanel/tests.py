b6199af133b98beccd23a807ec5f05dc0f01929e
Gabriel59 <ivascu.gabriel59@gmail.com>
2014-09-01 14:14:16 +0300

    Change field 'definition' into 'expression' in interface directory

--- wouso/interface/cpanel/tests.py
+++ wouso/interface/cpanel/tests.py
@@ -38,9 +38,9 @@ class CpanelViewsTest(WousoTest):
         self.client.login(username='admin', password='admin')
 
     def test_formulas_view(self):
-        Formula.objects.create(name='test_formula1', definition='points=1234',
+        Formula.objects.create(name='test_formula1', expression='points=1234',
                                description='First formula of the game')
-        Formula.objects.create(name='test_formula2', definition='points=1234',
+        Formula.objects.create(name='test_formula2', expression='points=1234',
                                description='Second formula of the game')
         response = self.client.get(reverse('formulas'))
         self.assertEqual(response.status_code, 200)
@@ -49,7 +49,7 @@ class CpanelViewsTest(WousoTest):
         self.assertContains(response, 'points=1234', 2)
 
     def test_edit_formula_view_get(self):
-        formula = Formula.objects.create(name='test_formula1', definition='points=1234',
+        formula = Formula.objects.create(name='test_formula1', expression='points=1234',
                                          description='First formula of the game')
         response = self.client.get(reverse('edit_formula', args=[formula.pk]))
         self.assertEqual(response.status_code, 200)
@@ -58,15 +58,15 @@ class CpanelViewsTest(WousoTest):
         self.assertContains(response, 'First formula of the game')
 
     def test_edit_formula_view_post(self):
-        formula = Formula.objects.create(name='test_formula1', definition='points=1234',
+        formula = Formula.objects.create(name='test_formula1', expression='points=1234',
                                          description='First formula of the game')
-        data = {'name': 'test_formula2', 'definition': 'points=4321',
+        data = {'name': 'test_formula2', 'expression': 'points=4321',
                 'description': 'Second formula of the game'}
         response = self.client.post(reverse('edit_formula', args=[formula.pk]), data)
         self.assertEqual(response.status_code, 302)
         updated_formula = Formula.objects.get(pk=formula.pk)
         self.assertEqual(updated_formula.name, 'test_formula2')
-        self.assertEqual(updated_formula.definition, 'points=4321')
+        self.assertEqual(updated_formula.expression, 'points=4321')
         self.assertEqual(updated_formula.description, 'Second formula of the game')
 
     def test_add_formula_view_get(self):
@@ -74,12 +74,12 @@ class CpanelViewsTest(WousoTest):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Add Formula')
         self.assertContains(response, 'Name:')
-        self.assertContains(response, 'Definition:')
+        self.assertContains(response, 'Expression:')
         self.assertContains(response, 'Description:')
 
     def test_add_formula_view_post(self):
         # Check the view with a valid form
-        data = {'name': 'test_formula1', 'definition': 'points=4321',
+        data = {'name': 'test_formula1', 'expression': 'points=4321',
                 'description': 'Test Formula'}
         response = self.client.post(reverse('add_formula'), data)
         self.assertEqual(response.status_code, 302)
@@ -87,7 +87,7 @@ class CpanelViewsTest(WousoTest):
         self.assertEqual(len(formulas), 1)
         formula = formulas[0]
         self.assertEqual(formula.name, 'test_formula1')
-        self.assertEqual(formula.definition, 'points=4321')
+        self.assertEqual(formula.expression, 'points=4321')
         self.assertEqual(formula.description, 'Test Formula')
 
         # Check the view with an invalid form
