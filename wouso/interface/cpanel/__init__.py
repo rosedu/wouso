3c222851a565d1b6a3d49fc9dd011572aa960361
Iulian Radu <iulian.radu67@gmail.com>
2014-09-06 00:49:32 +0300

    Rewrite get_cpanel_games().

--- wouso/interface/cpanel/__init__.py
+++ wouso/interface/cpanel/__init__.py
@@ -2,18 +2,27 @@ import os
 from wouso.core.game import get_games
 
 
-def get_cpanel_games():
-    games_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'games'))
+def has_cpanel_url(game):
+    games_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '../..', 'games'))
+    return os.path.exists(games_dir + '/' + game + '/cpanel_urls.py')
 
+
+def get_cpanel_games():
+    """
+     Returns a dict with games having a cpanel link looking like this:
+        gs({'specialquest':'Special Quest'})
+    """
     gs = {}
-    for g in os.listdir(games_dir):
-        if os.path.exists(games_dir + '/' + g + '/cpanel_urls.py'):
-            copy = g
-            if 'challenge' in g:
-                g = g.replace("challenge", " challenge")
-            if 'quest' in g:
-                g = g.replace("quest", " quest")
-            g = g.title()
-            gs[copy] = g
+    for game in get_games():
+        game = game.__name__.replace('Game', '')
+        if has_cpanel_url(game.lower()):
+            url = game.lower()
+            if 'challenge' in game:
+                game = game.replace("Challenge", " Challenge")
+            if 'quest' in game:
+                game = game.replace("Quest", " Quest")
+            game = game.title()
+            gs[url] = game
 
     return gs
