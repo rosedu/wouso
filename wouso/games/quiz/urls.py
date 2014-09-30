0c7c43c436bd2af307971b9b6fcec47026e7498c
Gabriel Ivascu <ivascu.gabriel59@gmail.com>
2014-09-30 18:32:38 +0300

    Add admin.py

--- wouso/games/quiz/urls.py
+++ wouso/games/quiz/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('wouso.games.quiz.views'
+urlpatterns = patterns('wouso.games.quiz.views',
     url(r'^$', 'index', name='quiz_index_view'),
 
 
