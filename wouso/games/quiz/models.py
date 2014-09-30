2c1a23233d5c1df8db756bcb42b6268f8cdb26bd
Iulian Radu <iulian.radu67@gmail.com>
2014-09-30 18:59:45 +0300

    Add Quiz category to Questions tab

--- wouso/games/quiz/models.py
+++ wouso/games/quiz/models.py
@@ -2,21 +2,27 @@ from django.db import models
 
 from wouso.core.user.models import Player
 from wouso.core.game.models import Game
+from wouso.core.qpool import register_category
 from wouso.core.qpool.models import Question
 
 
 class QuizUser(Player):
     """ Extension of the User object, customized for quiz """
-    my_question = models.ForeignKey(Question, related_name="MyQuestion", null=True)
+    my_question = models.ForeignKey(Question,
+                                    related_name="MyQuestion",
+                                    null=True)
 
 
 class QuizGame(Game):
     """ Each game must extend Game"""
     class Meta:
         proxy = True
+    QPOOL_CATEGORY = 'quiz'
 
     def __init__(self, *args, **kwargs):
         self._meta.get_field('verbose_name').default = "Quiz"
         self._meta.get_field('short_name').default = ""
         self._meta.get_field('url').default = "quiz_index_view"
         super(QuizGame, self).__init__(*args, **kwargs)
+
+register_category(QuizGame.QPOOL_CATEGORY, QuizGame)
