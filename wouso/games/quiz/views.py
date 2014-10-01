33c18b2631f3bf277a296dff0ab2e2f55e694087
Iulian Radu <iulian.radu67@gmail.com>
2014-10-01 14:39:31 +0300

    Add form view for quiz

--- wouso/games/quiz/views.py
+++ wouso/games/quiz/views.py
@@ -1,26 +1,36 @@
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.views.generic import View
-from django.shortcuts import redirect
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 
 from models import Quiz, QuizUser, QuizGame
-from wouso.core.ui import register_sidebar_block
+from forms import QuizForm
+from core.ui import register_sidebar_block
 
 
 class QuizView(View):
-
     def dispatch(self, request, *args, **kwargs):
         if QuizGame.disabled():
             return redirect('wouso.interface.views.homepage')
 
         profile = request.user.get_profile()
         self.quiz_user = profile.get_extension(QuizUser)
+        self.quiz = get_object_or_404(Quiz, pk=kwargs['id'])
 
         Quiz.create('lesson_one', False)
 
         return super(QuizView, self).dispatch(request, *args, **kwargs)
 
-index = login_required(QuizView.as_view())
+    def get(self, request, *args, **kwargs):
+        form = QuizForm(self.quiz)
+        return render_to_response('quiz/quiz.html',
+                                  {'quiz': self.quiz, 'form': form},
+                                  context_instance=RequestContext(request))
+
+
+quiz = login_required(QuizView.as_view())
+
 
 def sidebar_widget(context):
     user = context.get('user', None)
@@ -33,5 +43,6 @@ def sidebar_widget(context):
                             {'quser': quiz_user,
                              'quiz': QuizGame,
                              'id': 'quiz'})
+
 
 register_sidebar_block('quiz', sidebar_widget)
