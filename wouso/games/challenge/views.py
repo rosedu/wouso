52bfa1d69e35161d4e25beeb6e836724f2b0f450
Iulian Radu <iulian.radu67@gmail.com>
2014-10-19 10:06:42 +0300

    Revert "Make divisions work based on the challenge top"
    This reverts commit 3da29e1cef538040e78620eb46525a975f083849.

--- wouso/games/challenge/views.py
+++ wouso/games/challenge/views.py
@@ -170,9 +170,8 @@ def launch(request, to_id):
         return redirect('challenge_index_view')
 
     if not user_from.in_same_division(user_to):
-        messages.error(request, _('You are not in the same division'
-                                  '(within 20 positions of each other in the challenge top)'))
-        logging.info("Ready to unlock (not in same division).")
+        messages.error(request, _('You are not in the same division'))
+        logging.info("Ready to unlock (not in same divission).")
         lock.unlock()
         return redirect('challenge_index_view')
 
