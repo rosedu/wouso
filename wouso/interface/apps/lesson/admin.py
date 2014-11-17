87984ee2c84285168529b67b9dcb57cd1c57c29f
Gabriel Ivascu <ivascu.gabriel59@gmail.com>
2014-11-17 12:46:12 +0200

    Add lesson settings

--- wouso/interface/apps/lesson/admin.py
+++ wouso/interface/apps/lesson/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from interface.apps.lesson.models import Lesson
+from interface.apps.lesson.models import Lesson, LessonCategory
 
 admin.site.register(Lesson)
+admin.site.register(LessonCategory)
