6c48b956b13961abeddccf953006ca8206b17438
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2014-11-22 13:15:56 +0200

    qpool: Dynamically add/remove answers when adding a new question

--- wouso/resources/static/js/cpanel/qpool/qpool_new.js
+++ wouso/resources/static/js/cpanel/qpool/qpool_new.js
@@ -20,3 +20,18 @@ selectCategoryNode.change(function () {
     populateTags(currentCategory);
 });
 
+
+$(document).ready(function() {
+	var wrapper = $('#wrapper');
+	var i = $("#wrapper > div").length; // Set to the initial number of answers. We only increment i for uniqueness.
+	$('#add').click(function(e) {
+		e.preventDefault();
+		i++;
+		$('<div id="box_' + i +'" class="formfield"><div class="info-column"><a class="btn btn-sm btn-warning" id="remove"><span class="glyphicon glyphicon-minus"></span>Remove this answer</a></div><textarea name="answer_' + i +'" id="answer"></textarea><label class="mark" for="checkbox">Mark correct</label><input id="checkbox" type="checkbox" name="correct_' + i + '"/></div>').appendTo(wrapper);
+	});
+
+	$(wrapper).on('click', '#remove', function(e) {
+		e.preventDefault();
+		$(this).parent('div').parent('div').remove();
+	});
+});
