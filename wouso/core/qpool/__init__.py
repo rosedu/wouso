b3afd9dd290deba13902e00b6823eab942adfbff
Iulian Radu <iulian.radu67@gmail.com>
2014-11-28 12:34:56 +0200

    Fix import

--- wouso/core/qpool/__init__.py
+++ wouso/core/qpool/__init__.py
@@ -1,20 +1,20 @@
 # TODO: check usage of these functions and rename/refactor
 from django.db import models
 from django.db.models import Q
-from core.qpool.models import Question
+from wouso.core.qpool.models import Question
 
 
 def get_questions_with_tags(tlist, select='any', active_only=True, endorsed_only=True):
     if isinstance(tlist, str):
-        result = models.Question.objects.filter(tags__name=tlist)
+        result = Question.objects.filter(tags__name=tlist)
     else:
         if select == 'any':
             tfilters = Q(tags__name=tlist[0])
             for t in tlist[1:]:
                 tfilters = tfilters | Q(tags__name=t)
-            result = models.Question.objects.filter(tfilters)
+            result = Question.objects.filter(tfilters)
         elif select == 'all':
-            result = models.Question.objects.filter(tags__name=tlist[0])
+            result = Question.objects.filter(tags__name=tlist[0])
             for t in tlist[1:]:
                 result = result.filter(tags__name=t)
     # more filtering
@@ -27,11 +27,11 @@ def get_questions_with_tags(tlist, select='any', active_only=True, endorsed_only
 def get_questions_with_category(category='all', active_only=True, endorsed_only=False):
     ''' can be called with all, string for category name or instance of Category '''
     if category == 'all':
-        result = models.Question.objects.all()
+        result = Question.objects.all()
     elif isinstance(category, str):
-        result = models.Question.objects.filter(category__name=category)
+        result = Question.objects.filter(category__name=category)
     elif isinstance(category, models.Category):
-        result = models.Question.objects.filter(category=category)
+        result = Question.objects.filter(category=category)
 
     # filtering
     if active_only:
@@ -43,7 +43,7 @@ def get_questions_with_category(category='all', active_only=True, endorsed_only=
 
 
 def get_questions_with_tag_and_category(tags='all', category='all', active_only=True, endorsed_only=False):
-    result = models.Question.objects.filter(tags__name__in=tags, category__name=category)
+    result = Question.objects.filter(tags__name__in=tags, category__name=category)
 
     # filtering
     if active_only:
