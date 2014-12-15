16b161b486881fd3bb52193bb5bf048affd50664
Iulian Radu <iulian.radu67@gmail.com>
2014-12-15 13:09:24 +0200

    [Forum] Final touch.

--- wouso/interface/forum/models.py
+++ wouso/interface/forum/models.py
@@ -79,7 +79,6 @@ class Post(models.Model):
     user = models.ForeignKey(ForumUser)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    indentation = models.IntegerField(default=0)
     text = RichTextField()
 
     class Meta:
--- wouso/interface/forum/urls.py
+++ wouso/interface/forum/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('wouso.interface.forum.views',
-    url(r'^$', 'category', name='overview'),
+    url(r'^$', 'category', name='forums_overview'),
     url(r'^(?P<pk>\d+)/$', 'forum', name='forum'),
     url(r'^(?P<forum_id>\d+)/create/$', 'topic_create', name='topic_create'),
     url(r'^topic/(?P<pk>\d+)/$', 'topic', name='topic'),
