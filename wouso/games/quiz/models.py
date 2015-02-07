rosedu/wouso

commit 139da4ac23e2403edf85870fe1f4d310e0ee7234

Refactor

wouso/games/quiz/models.py
@@ -13,50 +13,83 @@ class QuizException(Exception):
     pass
 
 
-class QuizUser(Player):
-    """ Extension of the User object, customized for quiz """
-    my_question = models.ForeignKey(Question,
-                                    related_name="MyQuestion",
-                                    null=True)
-    # time when user started quiz
-    start = models.DateTimeField(null=True, blank=True)
-    # ID of current started quiz
-    started_quiz_id = models.IntegerField(default=0)
-
-Player.register_extension('quiz', QuizUser)
-
-
 class Quiz(models.Model):
+    CHOICES = {
+        ('A', 'ACTIVE'),
+        ('I', 'INACTIVE'),  # Active in future
+        ('E', 'EXPIRED')
+    }
     name = models.CharField(max_length=100)
     number_of_questions = models.IntegerField(default=5)
     time_limit = models.IntegerField(default=300)
     questions = models.ManyToManyField(Question)
-    owner = models.ForeignKey(Game, null=True, blank=True)
 
     start = models.DateTimeField()
     end = models.DateTimeField()
 
-    players = models.ManyToManyField(QuizUser)
+    owner = models.ForeignKey(Game, null=True, blank=True)
+
+    status = models.CharField(max_length=1, choices=CHOICES)
+
+    def set_active(self):
+        self.status = 'A'
+        self.save()
+
+    def set_inactive(self):
+        self.status = 'I'
+        self.save()
+
+    def set_started(self):
+        self.status = 'S'
+        self.save()
+
+    def set_played(self):
+        self.status = 'P'
+        self.save()
+
+    def set_expired(self):
+        self.status = 'E'
+        self.save()
+
+    def is_active(self):
+        return self.status == 'A'
+
+    def is_inactive(self):
+        return self.status == 'I'
+
+    def is_started(self):
+        return self.status == 'S'
+
+    def is_played(self):
+        return self.status == 'P'
+
+    def is_expired(self):
+        return self.status == 'E'
 
     @property
     def elapsed(self):
         if self.end < datetime.now():
             return True
         return False
+    #
+    # @property
+    # def is_active(self):
+    #     now = datetime.now()
+    #     if self.end < now:
+    #         return False
+    #     elif self.start > now:
+    #         return False
+    #     return True
 
-    @property
-    def is_active(self):
-        now = datetime.now()
-        if self.end < now:
-            return False
-        elif self.start > now:
-            return False
-        return True
+    # @property
+    # def active_quizzes(self):
+    #     active_quizzes = [q for q in self.objects.all() if q.is_active()]
+    #     return active_quizzes
 
     @classmethod
-    def get_active_quizzes(self):
-        active_quizzes = [q for q in self.objects.all() if q.is_active]
-        return active_quizzes
+    def played_quizzes(self):
+        played_quizzes = [q for q in self.objects.all() if q.time_for_user() == 0]
+        return played_quizzes
 
     @classmethod
     def calculate_points(cls, responses):
@@ -94,12 +127,13 @@ class Quiz(models.Model):
     def add_player(self, player):
         """ Add player to the list of players which have played the quiz
         """
-        self.players.add(player)
+        # self.players.add(player)
 
     def set_start(self, user):
         """ Set quiz start time for user
         """
         user.start = datetime.now()
+        user.quiz.set_started()
         user.save()
 
     def is_started_for_user(self, user):
@@ -138,3 +172,36 @@ class QuizGame(Game):
         super(QuizGame, self).__init__(*args, **kwargs)
 
 register_category(QuizGame.QPOOL_CATEGORY, QuizGame)
+
+
+class QuizUser(Player):
+    """ Extension of the User object, customized for quiz """
+    # quiz = models.ForeignKey(Quiz, null=True)
+
+    # time when user started quiz
+    start = models.DateTimeField(null=True, blank=True)
+    # ID of current started quiz
+    started_quiz_id = models.IntegerField(default=0)
+    quizzes = models.ManyToManyField(Quiz, through='UserToQuiz')
+    # score = models.IntegerField(default=0)
+    # quizzes = models.ForeignKey(Quiz, null=True, blank=True)
+
+    @property
+    def active_quizzes(self):
+        active_quizzes = [q for q in Quiz.objects.all() if q.is_active()]
+        return active_quizzes
+
+
+Player.register_extension('quiz', QuizUser)
+
+
+class UserToQuiz(models.Model):
+    CHOICES = {
+        ('P', 'PLAYED'),
+        ('R', 'RUNNING'),
+        ('N', 'NOT STARTED')
+    }
+    user = models.ForeignKey(QuizUser)
+    quiz = models.ForeignKey(Quiz)
+    state = models.CharField(max_length=1, choices=CHOICES, default='N')
+    score = models.IntegerField(default=-1)

wouso/games/quiz/cpanel_views.py
@@ -28,11 +28,13 @@ class AddQuizView(CreateView):
 add_quiz = permission_required('config.change_setting')(
     AddQuizView.as_view())
 
+
 @permission_required('config.change_setting')
 def delete_quiz(request, id):
     quiz = get_object_or_404(Quiz, pk=id)
     quiz.delete()
     return redirect('list_quizzes')
+
 
 class EditQuizView(UpdateView):
     template_name = 'quiz/cpanel/edit_quiz.html'

wouso/games/quiz/forms.py
@@ -1,9 +1,10 @@
 from random import shuffle
+from datetime import datetime
 from django import forms
 from core.qpool import get_questions_with_tag_and_category
 from core.qpool.models import Tag, Question
 from games.quiz.models import QuizException
-from wouso.games.quiz.models import Quiz
+from wouso.games.quiz.models import Quiz, QuizUser, UserToQuiz
 from bootstrap3_datetime import widgets
 
 
@@ -62,7 +63,7 @@ class AddQuizForm(forms.ModelForm):
         widgets = {'start': widgets.DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"}),
                    'end': widgets.DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"})
         }
-        exclude = ['questions', 'owner', 'players']
+        exclude = ['questions', 'owner', 'players', 'status']
 
     def __init__(self, *args, **kwargs):
         super(AddQuizForm, self).__init__(*args, **kwargs)
@@ -72,7 +73,7 @@ class AddQuizForm(forms.ModelForm):
         self.fields['tags'].label = "Question tags"
         self.fields['time_limit'].label = "Time limit (seconds)"
 
-    def save(self):
+    def save(self, commit=True):
         data = self.cleaned_data
 
         # Get a list of questions from the Quiz category with tags selected
@@ -87,5 +88,15 @@ class AddQuizForm(forms.ModelForm):
 
         self.instance.save()
         self.instance.questions = questions_qs
+
+        # Set status based on current date and time (Active, Inactive, Expired)
+        now = datetime.now()
+        if data['start'] <= now <= data['end']:
+            self.instance.status = 'A'
+        elif data['start'] > now:
+            self.instance.status = 'I'
+        elif data['end'] < now:
+            self.instance.status = 'E'
+        self.instance.save()
 
         return self.instance

wouso/games/quiz/templates/quiz/index.html
@@ -5,7 +5,7 @@
 
 {% block sectioncontent %}
 
-{% if active_quizzes or inactive_quizzes %}
+{% if active_quizzes %}
     <h3>Active Quizzes</h3>
     <ul>
         {% for q in active_quizzes %}
@@ -14,12 +14,21 @@
             </li>
         {% endfor %}
     </ul>
-    
-    <h3>Inactive Quizzes</h3>
+
+    <h3>Played quizzes</h3>
     <ul>
-        {% for q in inactive_quizzes %}
+        {% for q in played_quizzes %}
             <li>
-                {{ q.name }}
+                <a href="{% url quiz_view q.id %}">{{ q.name }} {{ q.points }}</a>
+            </li>
+        {% endfor %}
+    </ul>
+
+    <h3>Expired quizzes</h3>
+    <ul>
+        {% for q in expired_quizzes %}
+            <li>
+                <a href="{% url quiz_view q.id %}">{{ q.name }}</a>
             </li>
         {% endfor %}
     </ul>

wouso/games/quiz/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.contrib import messages
 from django.utils.translation import ugettext as _
 from django.contrib.auth.decorators import login_required
@@ -8,7 +9,7 @@ from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.core.urlresolvers import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
 
-from models import Quiz, QuizUser, QuizGame
+from models import Quiz, QuizUser, QuizGame, UserToQuiz
 from forms import QuizForm
 from wouso.core.ui import register_sidebar_block
 
@@ -16,12 +17,22 @@ from wouso.core.ui import register_sidebar_block
 @login_required
 def index(request):
     """ Shows all quizzes related to the current user """
-    quizzes = Quiz.objects.filter()
-    inactive_quizzes = [q for q in quizzes if q.elapsed]
-    active_quizzes = [q for q in quizzes if q.is_active]
-    return render_to_response('quiz/index.html',
-                              {'active_quizzes': active_quizzes,
-                               'inactive_quizzes': inactive_quizzes},
+    profile = request.user.get_profile()
+    quiz_user = profile.get_extension(QuizUser)
+
+    for q in Quiz.objects.all():
+        try:
+            obj = UserToQuiz.objects.get(user=quiz_user, quiz=q)
+        except UserToQuiz.DoesNotExist:
+            obj = UserToQuiz(user=quiz_user, quiz=q)
+            obj.save()
+
+    for q in quiz_user.active_quizzes:
+        print q.name
+
+    return render_to_response('quiz/index.html', {},
+                              # {'active_quizzes': quiz_user.active_quizzes},
+                              #  'inactive_quizzes': inactive_quizzes},
                               context_instance=RequestContext(request))
 
 
@@ -32,10 +43,11 @@ class QuizView(View):
 
         profile = request.user.get_profile()
         self.quiz_user = profile.get_extension(QuizUser)
-        self.quiz = get_object_or_404(Quiz, pk=kwargs['id'])
+        # self.quiz_user.quiz = get_object_or_404(Quiz, pk=kwargs['id'])
+
 
         # check if user has already played quiz
-        if self.quiz_user in self.quiz.players.all():
+        if self.quiz_user.quiz.is_played():
             messages.error(request, _('You have already submitted this'
                                       ' quiz!'))
             return HttpResponseRedirect(reverse('quiz_index_view'))
@@ -49,33 +61,34 @@ class QuizView(View):
         return super(QuizView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        form = QuizForm(self.quiz)
+        form = QuizForm(self.quiz_user.quiz)
 
-        if not self.quiz.is_started_for_user(self.quiz_user):
-            self.quiz.set_start(self.quiz_user)
+        if not self.quiz_user.quiz.is_started_for_user(self.quiz_user):
+            self.quiz_user.quiz.set_start(self.quiz_user)
 
-        seconds_left = self.quiz.time_for_user(self.quiz_user)
+        seconds_left = self.quiz_user.quiz.time_for_user(self.quiz_user)
         # set ID of current started quiz
         self.quiz_user.started_quiz_id = kwargs['id']
         self.quiz_user.save()
 
         return render_to_response('quiz/quiz.html',
-                                  {'quiz': self.quiz, 'form': form, 'seconds_left': seconds_left},
+                                  {'quiz': self.quiz_user.quiz, 'form': form, 'seconds_left': seconds_left},
                                   context_instance=RequestContext(request))
 
     def post(self, request, **kwargs):
-        form = QuizForm(self.quiz, request.POST)
+        form = QuizForm(self.quiz_user.quiz, request.POST)
         results = form.get_response()
         form.check_self_boxes()
 
         # add player to the list of quiz players
-        self.quiz.add_player(self.quiz_user)
+        self.quiz_user.quiz.add_player(self.quiz_user)
         # reset start time and ID of current started quiz
-        self.quiz.reset(self.quiz_user)
+        self.quiz_user.quiz.reset(self.quiz_user)
+        self.quiz_user.quiz.set_played()
 
         results = Quiz.calculate_points(results)
         return render_to_response(('quiz/result.html'),
-                                  {'quiz': self.quiz, 'points': results['points']},
+                                  {'quiz': self.quiz_user.quiz, 'points': results['points']},
                                   context_instance=RequestContext(request))
 
 
@@ -87,10 +100,10 @@ def sidebar_widget(context):
     if not user or not user.is_authenticated():
         return ''
 
-    number_of_active_quizzes = Quiz.get_active_quizzes().__len__()
+    # number_of_active_quizzes = Quiz.get_active_quizzes().__len__()
 
-    return render_to_string('quiz/sidebar.html',
-                            {'number_of_active_quizzes': number_of_active_quizzes}
+    return render_to_string('quiz/sidebar.html', {},
+                            # {'number_of_active_quizzes': number_of_active_quizzes}
                              )
 
 

commit bb269ad55f3decfd3ca0496037084cf049cd9296

Fix database error when doing wouso setup
When doing ./manage.py wousoctl --setup, you would get an error such as
this:

django.db.utils.DatabaseError: no such table: config_settings

This is due to a rather nasty situation of loading some modules ahead of
the others. As such proper database tables would not be created. This
bug was introduced in commit 139da4ac23e2403edf85870fe1f4d310e0ee7234.
It is caused by placing the register_category() call ahead of the
definition of the QuizUser class. The bug is fixed by moving the call
after the definition of the QuizUser class.

wouso/games/quiz/models.py
@@ -117,9 +117,6 @@ class QuizGame(Game):
         super(QuizGame, self).__init__(*args, **kwargs)
 
 
-register_category(QuizGame.QPOOL_CATEGORY, QuizGame)
-
-
 class QuizUser(Player):
     """
      Extension of the User object, customized for quiz
@@ -146,6 +143,8 @@ class QuizUser(Player):
 
 
 Player.register_extension('quiz', QuizUser)
+
+register_category(QuizGame.QPOOL_CATEGORY, QuizGame)
 
 
 class UserToQuiz(models.Model):