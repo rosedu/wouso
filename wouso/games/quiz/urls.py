f3b320ab03777f3e070d81033205fa863314a996
Iulian Radu <iulian.radu67@gmail.com>
2015-02-10 21:50:51 +0200

    [quiz] Add quiz categories page

--- wouso/games/quiz/urls.py
+++ wouso/games/quiz/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('wouso.games.quiz.views',
     url(r'^$', 'index', name='quiz_index_view'),
+    url(r'^cat/(?P<id>\d+)/$', 'category', name='quiz_category_view'),
     url(r'^(?P<id>\d+)/$', 'quiz', name='quiz_view'),
 )
