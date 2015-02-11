69a00075b786d0073e8ddbaa085230027e05c7ec
Iulian Radu <iulian.radu67@gmail.com>
2015-02-11 12:19:10 +0200

    [lesson] Add logo field for category and tag

---
+++ wouso/interface/apps/lesson/migrations/0003_auto__add_field_lessoncategory_logo__add_field_lessontag_logo.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'LessonCategory.logo'
+        db.add_column('lesson_lessoncategory', 'logo',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'LessonTag.logo'
+        db.add_column('lesson_lessontag', 'logo',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'LessonCategory.logo'
+        db.delete_column('lesson_lessoncategory', 'logo')
+
+        # Deleting field 'LessonTag.logo'
+        db.delete_column('lesson_lessontag', 'logo')
+
+
+    models = {
+        'game.game': {
+            'Meta': {'object_name': 'Game'},
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'primary_key': 'True'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'verbose_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'})
+        },
+        'lesson.lesson': {
+            'Meta': {'object_name': 'Lesson'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'content': ('ckeditor.fields.RichTextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'quiz': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['quiz.Quiz']", 'null': 'True', 'blank': 'True'}),
+            'quiz_show_time': ('django.db.models.fields.IntegerField', [], {'default': '5'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'lessons'", 'null': 'True', 'to': "orm['lesson.LessonTag']"}),
+            'youtube_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'lesson.lessoncategory': {
+            'Meta': {'object_name': 'LessonCategory'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'order': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '1000', 'blank': 'True'})
+        },
+        'lesson.lessontag': {
+            'Meta': {'object_name': 'LessonTag'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'tags'", 'null': 'True', 'to': "orm['lesson.LessonCategory']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'quiz.quiz': {
+            'Meta': {'object_name': 'Quiz'},
+            'another_chance': ('django.db.models.fields.IntegerField', [], {'default': '7'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['quiz.QuizCategory']", 'null': 'True', 'blank': 'True'}),
+            'end': ('django.db.models.fields.DateTimeField', [], {}),
+            'gold_reward': ('django.db.models.fields.IntegerField', [], {'default': '30'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['game.Game']", 'null': 'True', 'blank': 'True'}),
+            'points_reward': ('django.db.models.fields.IntegerField', [], {'default': '100'}),
+            'start': ('django.db.models.fields.DateTimeField', [], {}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'tags': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'time_limit': ('django.db.models.fields.IntegerField', [], {'default': '300'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'quiz.quizcategory': {
+            'Meta': {'object_name': 'QuizCategory'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['lesson']
