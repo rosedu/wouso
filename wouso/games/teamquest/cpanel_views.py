bc968664eb9ee73a86aeb0c56d1afad0129951f2
Popa Razvan <razz.popa@gmail.com>
2015-04-07 11:55:01 +0300

    [TeamQuest] Create list_quests template for Team Quest in cpanel

--- wouso/games/teamquest/cpanel_views.py
+++ wouso/games/teamquest/cpanel_views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.decorators import permission_required
 from django.views.generic import ListView
 
-from models import TeamQuest
+from wouso.games.teamquest.models import TeamQuest
 
 
-class QuestsView(ListView):
+class HomeView(ListView):
     model = TeamQuest
-    template_name = 'teamquest/quests.html'
     context_object_name = 'quests'
+    template_name = 'teamquest/cpanel/list_quests.html'
 
 
-quests = permission_required('config.change_setting')(QuestsView.as_view())
+quests = permission_required('config.change_setting')(HomeView.as_view())
