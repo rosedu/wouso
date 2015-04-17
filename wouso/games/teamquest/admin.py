c7b5e92bf1571b4ccad38d0d755502dd6636712a
Razvan Chitu <razvan3895@yahoo.com>
2015-04-17 14:13:45 +0300

    [TeamQuest] Register models in admin.py

--- wouso/games/teamquest/admin.py
+++ wouso/games/teamquest/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from models import TeamQuest, TeamQuestUser, TeamQuestGroup, TeamQuestInvitation, TeamQuestInvitationRequest
+from models import *
 
+admin.site.register(TeamQuestUser)
+admin.site.register(TeamQuestGroup)
 admin.site.register(TeamQuestInvitation)
 admin.site.register(TeamQuestInvitationRequest)
+admin.site.register(TeamQuest)
+admin.site.register(TeamQuestLevel)
+admin.site.register(TeamQuestStatus)
+admin.site.register(TeamQuestLevelStatus)
+admin.site.register(TeamQuestQuestion)
