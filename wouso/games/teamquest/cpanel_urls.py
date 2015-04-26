cc44a3da21c17ee5b161064cca0d63c927df3748
Popa Razvan <razz.popa@gmail.com>
2015-04-26 13:58:08 +0300

    [TeamQuest] Create groups tab in cpanel

--- wouso/games/teamquest/cpanel_urls.py
+++ wouso/games/teamquest/cpanel_urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('wouso.games.teamquest.cpanel_views',
-    url(r'^$', 'quests', name='teamquest_home'))
+    url(r'^$', 'quests', name='teamquest_home'),
+    url(r'^groups/$', 'groups', name='teamquest_groups'))
--- wouso/games/teamquest/cpanel_views.py
+++ wouso/games/teamquest/cpanel_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.views.generic import ListView
 
-from wouso.games.teamquest.models import TeamQuest
+from wouso.games.teamquest.models import TeamQuest, TeamQuestGroup
 
 
 class QuestsView(ListView):
@@ -11,3 +11,12 @@ class QuestsView(ListView):
 
 
 quests = permission_required('config.change_setting')(QuestsView.as_view())
+
+
+class GroupsView(ListView):
+    model = TeamQuestGroup
+    context_object_name = 'groups'
+    template_name = 'teamquest/cpanel/list_groups.html'
+
+
+groups = permission_required('config.change_setting')(GroupsView.as_view())
