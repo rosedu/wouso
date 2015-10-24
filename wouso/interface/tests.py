9295b37904fb669f5d28f344788b385895e94d20
vladcruceru <cruceru_vlad@yahoo.com>
2015-10-24 03:43:16 +0300

    added test for exchange button

--- wouso/interface/tests.py
+++ wouso/interface/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from wouso.core.tests import WousoTest
+from wouso.core.config.models import BoolSetting
 
 class TestInterface(WousoTest):
     def test_homepage_anonymous(self):
@@ -20,9 +21,27 @@ class TestInterface(WousoTest):
         response = self.client.get(reverse('player_profile', kwargs=dict(id=admin.id)))
         self.assertEqual(response.status_code, 200)
 
-    def test_achievements_link(safe):
+    def test_achievements_link(self):
         player = self._get_player()
         self.client.login(username=player.user.username, password='test')
         response = self.client.get('/hub/')
 
         self.assertTrue('Achievements' in response.content)
+
+    def test_exchange_button_appear(self):
+        player = self._get_player()
+        self.client.login(username=player.user.username, password='test')
+        setting = BoolSetting('disable-Bazaar-Exchange')
+        setting.set_value(False)
+
+        response = self.client.get('/bazaar/')
+        self.assertTrue('Exchange' in response.content)
+
+    def test_exchange_button_not_appear(self):
+        player = self._get_player()
+        self.client.login(username=player.user.username, password='test')
+        setting = BoolSetting('disable-Bazaar-Exchange')
+        setting.set_value(True)
+
+        response = self.client.get('/bazaar/')
+        self.assertFalse('Exchange' in response.content)
