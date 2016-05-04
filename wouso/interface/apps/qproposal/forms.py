1d7f96e21f13695ad2b666b0362ba6c672465956
ioanachirca <jo.chirca@gmail.com>
2016-05-04 14:48:03 +0300

    Add 'ProposedQuestionForm'

--- wouso/interface/apps/qproposal/forms.py
+++ wouso/interface/apps/qproposal/forms.py
@@ -9,7 +9,7 @@ class QuestionForm(forms.Form):
     category = forms.ChoiceField(choices=[(cat.name, cat.name) for cat in Category.objects.all().exclude(name='proposed')], widget=forms.Select(attrs={
         'id': 'select-category'}))
 
-    """def __init__(self, nr_ans=6, data=None):
+    def __init__(self, nr_ans=6, data=None):
         super(QuestionForm, self).__init__(data)
         self.nr_ans = nr_ans
         for i in range(nr_ans):
@@ -21,21 +21,6 @@ class QuestionForm(forms.Form):
                         choices=[(tag.name, tag.name) for tag in alltags],
                         widget=forms.SelectMultiple(attrs={
                             'id': 'select-tags'}), required=False)
-"""
-    def __init__(self, nr_ans=2, data=None):
-        super(QuestionForm, self).__init__(data)
-        self.nr_ans = nr_ans
-        for i in range(nr_ans):
-            self.fields['answer_%d' % i] = forms.CharField(max_length=100,
-                                    widget=forms.Textarea, required=False)
-            self.fields['correct_%d' % i] = forms.BooleanField(required=False, label="Correct?")
-        alltags = Tag.objects.all().exclude(name__in=['qotd', 'challenge', 'quest'])
-        self.fields['tags'] = forms.MultipleChoiceField(
-                        choices=[(tag.name, tag.name) for tag in alltags],
-                        widget=forms.SelectMultiple(attrs={
-                            'id': 'select-tags'}), required=False)
-
-    
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -50,4 +35,33 @@ class QuestionForm(forms.Form):
             raise forms.ValidationError("Must have only one correct answer!")
         elif nr == 1 and answer_type == 'C':
             raise forms.ValidationError("Must have more than one correct answer!")
+        return cleaned_data
+
+class ProposedQuestionForm(forms.Form):
+
+    text = forms.CharField(max_length=500, widget=forms.Textarea)
+    category = forms.ChoiceField(choices=[(cat.name, cat.name) for cat in Category.objects.all()], widget=forms.Select(attrs={
+        'id': 'select-category'}))
+
+    def __init__(self, nr_ans=6, data=None):
+        super(ProposedQuestionForm, self).__init__(data)
+        self.nr_ans = nr_ans
+        for i in range(nr_ans):
+            self.fields['answer_%d' % i] = forms.CharField(max_length=100,
+                                    widget=forms.Textarea, required=False)
+            self.fields['correct_%d' % i] = forms.BooleanField(required=False, label="Correct?")
+        alltags = Tag.objects.all().exclude(name__in=['qotd', 'challenge', 'quest'])
+        self.fields['tags'] = forms.MultipleChoiceField(
+                        choices=[(tag.name, tag.name) for tag in alltags],
+                        widget=forms.SelectMultiple(attrs={
+                            'id': 'select-tags'}), required=False)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        nr = 0
+        for i in range(self.nr_ans):
+            if cleaned_data.get('correct_%d' % i) is True:
+                nr += 1
+        if nr == 0:
+            raise forms.ValidationError("No correct answer!")
         return cleaned_data
